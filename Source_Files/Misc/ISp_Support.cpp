824623b2c2e8b2ec230a61aefc82371d14b66997
lpetrich <lpetrich@users.sourceforge.net>
2000-07-30 05:18:39 +0000

    Did some rewriting and added support for the full range of global actions.
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@94 a75d40c9-0969-4759-85e5-97cb871b7ed4

--- Source_Files/Misc/ISp_Support.cpp
+++ Source_Files/Misc/ISp_Support.cpp
@@ -1,3 +1,11 @@
+/*
+	Loren Petrich: This code was originally written by someone else;
+	I've modified it to improve InputSprocket support.
+	
+July 29, 2000 (Loren Petrich):
+	Added icons and given them new symbolic names
+*/
+
 
 /* marathon includes */
 #include "ISp_Support.h"
@@ -20,52 +28,79 @@ const OSType kCreatorCode		= '26.A';
 const OSType kSubCreatorCode	= 'BENT';
 #define	kResourceID_setl			128
 
+// LP change: renamed
 enum
 {
-	kIconSuiteID_XThrust 		= 129, 
-	kIconSuiteID_YThrust 		= 130,
-	kIconSuiteID_ZThrust 		= 131,
-	kIconSuiteID_XRotation		= 132,
-	kIconSuiteID_YRotation		= 133,
-	kIconSuiteID_ZRotation		= 134,
-	kIconSuiteID_Look 			= 159,
-	kIconSuiteID_Fire			= 141,
-	kIconSuiteID_SecondaryFire	= 142,
-	kIconSuiteID_ThrustForward	= 147,
-	kIconSuiteID_ThrustBackward	= 148,
-	kIconSuiteID_Pause			= 144,
-	kIconSuiteID_Scroll			= 140,
-	kIconSuiteID_LookLeft		= 149,
-	kIconSuiteID_LookRight		= 150,
-	kIconSuiteID_DeltaX			= 256,
-	kIconSuiteID_DeltaY			= 257
+	Icon_FirePrimary	 = 1000,
+	Icon_FireSecondary	 = 1001,
+	Icon_MoveForward	 = 1002,
+	Icon_MoveBackward	 = 1003,
+	Icon_TurnLeft		 = 1004,
+	Icon_TurnRight		 = 1005,
+	Icon_MoveLeft		 = 1006,
+	Icon_MoveRight		 = 1007,
+	Icon_LookLeft		 = 1008,
+	Icon_LookRight		 = 1009,
+	Icon_LookUp			 = 1010,
+	Icon_LookDown		 = 1011,
+	Icon_LookForward	 = 1012,
+	Icon_PrevWeapon		 = 1013,
+	Icon_NextWeapon		 = 1014,
+	Icon_Action			 = 1015,
+	Icon_RunNotWalk		 = 1016,
+	Icon_SideNotTurn	 = 1017,
+	Icon_LookNotTurn	 = 1018,
+	Icon_AutoMap		 = 1019,
+	Icon_Microphone		 = 1020,
+	Icon_Quit			 = 1021,
+	Icon_LookHorizontal	 = 1100,
+	Icon_LookVertical	 = 1101,
+	Icon_Move			 = 1102
 };
 
-
+// LP change: renamed
 enum {
-	kBtnFirePrimary,
-	kBtnMicrophone = 17,
-	kBtnQuit,
+	// All global events, those transmitted across the network.
+	// They are used to compose action_flags
+	Button_FirstGlobal = 0,
+	Button_LastGlobal = 20,
+	// All local events, those that have only local effects
+	Button_FirstLocal = 21,
+	Button_LastLocal = 21,
 	kAxisHoriz,
 	kAxisVert,
 	kAxisMove,
-	kNeedCount = 22
+	kNeedCount	// LP: calculated automatically
 };
 
-static Boolean						canDoISp = true;
-static Boolean						active = false;
-static long							gElementActions[kNeedCount] = {
-											_left_trigger_state, _right_trigger_state, 
-											_moving_forward, 
-											_moving_backward, _turning_left, 
-											_turning_right, _sidestepping_left,
-											_sidestepping_right, _looking_left,
-											_looking_right, _looking_up,
-											_looking_down, _looking_center,
-											_cycle_weapons_backward, _cycle_weapons_forward,
-											_action_trigger_state, _toggle_map, _microphone_button, 0, 0, 0, 0};
+static Boolean	canDoISp = true;
+static Boolean	active = false;
+static long		gElementActions[kNeedCount] =
+					{
+					_left_trigger_state, _right_trigger_state, 
+					_moving_forward, _moving_backward,
+					_turning_left, _turning_right,
+					_sidestepping_left, _sidestepping_right,
+					_looking_left, _looking_right,
+					_looking_up, _looking_down, _looking_center,
+					_cycle_weapons_backward, _cycle_weapons_forward,
+					_action_trigger_state, _run_dont_walk,
+					_sidestep_dont_turn, _look_dont_turn,
+					_toggle_map, _microphone_button,
+					0, 0, 0, 0};
+
 ISpElementListReference		gVirtualList = NULL;
-ISpElementReference			gVirtualElements[kNeedCount] = {nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil};
+ISpElementReference			gVirtualElements[kNeedCount] =
+	{	// LP: prettyprinted to make them easier to match with gElementActions members
+		// Global actions
+		nil, nil, nil, nil,
+		nil, nil, nil, nil,
+		nil, nil, nil, nil, nil,
+		nil, nil, nil, nil,
+		nil, nil, nil, nil,
+		// Local actions, axis stuff
+		nil, nil, nil, nil
+	};
 
 // LP change:
 #define kISpNeedFlag_Delta_AlreadyButton kISpNeedFlag_Axis_AlreadyButton
@@ -74,30 +109,33 @@ ISpElementReference			gVirtualElements[kNeedCount] = {nil, nil, nil, nil, nil, n
 static ISpNeed gNeeds[kNeedCount] =
 {
 	// Standard Button Items
-	{ "\pFire Primary", 	kIconSuiteID_LookRight, 	0, kISpElementKind_Button,		kISpElementLabel_Btn_Fire,			kISpNeedFlag_Button_AlreadyAxis | kISpNeedFlag_Button_ActiveWhenDown, 0, 0, 0	},
-	{ "\pFire Secondary", 	kIconSuiteID_LookRight, 	0, kISpElementKind_Button,		kISpElementLabel_Btn_SecondaryFire,	kISpNeedFlag_Button_AlreadyAxis | kISpNeedFlag_Button_ActiveWhenDown, 0, 0, 0	},
-	{ "\pMove Forward",		kIconSuiteID_ZThrust, 		0, kISpElementKind_Button,		kISpElementLabel_Btn_MoveForward,	kISpNeedFlag_Button_AlreadyAxis | kISpNeedFlag_Button_ActiveWhenDown, 0, 0, 0	},
-	{ "\pMove Backward",	kIconSuiteID_ZThrust, 		0, kISpElementKind_Button,		kISpElementLabel_Btn_MoveBackward,	kISpNeedFlag_Button_AlreadyAxis | kISpNeedFlag_Button_ActiveWhenDown, 0, 0, 0	},
-	{ "\pLook Left", 		kIconSuiteID_LookLeft,		0, kISpElementKind_Button,		kISpElementLabel_Btn_TurnLeft,		kISpNeedFlag_Button_AlreadyAxis | kISpNeedFlag_Button_ActiveWhenDown, 0, 0, 0	},
-	{ "\pLook Right", 		kIconSuiteID_LookRight, 	0, kISpElementKind_Button,		kISpElementLabel_Btn_TurnRight,		kISpNeedFlag_Button_AlreadyAxis | kISpNeedFlag_Button_ActiveWhenDown, 0, 0, 0	},
-	{ "\pSidestep Left", 	kIconSuiteID_LookRight, 	0, kISpElementKind_Button,		kISpElementLabel_Btn_SlideLeft,		kISpNeedFlag_Button_AlreadyAxis | kISpNeedFlag_Button_ActiveWhenDown, 0, 0, 0	},
-	{ "\pSidestep Right", 	kIconSuiteID_LookRight, 	0, kISpElementKind_Button,		kISpElementLabel_Btn_SlideRight,	kISpNeedFlag_Button_AlreadyAxis | kISpNeedFlag_Button_ActiveWhenDown, 0, 0, 0	},
-	{ "\pGlance Left",	 	kIconSuiteID_LookRight, 	0, kISpElementKind_Button,		kISpElementLabel_Btn_LookLeft,		kISpNeedFlag_Button_AlreadyAxis | kISpNeedFlag_Button_ActiveWhenDown, 0, 0, 0	},
-	{ "\pGlance Right", 	kIconSuiteID_LookRight, 	0, kISpElementKind_Button,		kISpElementLabel_Btn_LookRight,		kISpNeedFlag_Button_AlreadyAxis | kISpNeedFlag_Button_ActiveWhenDown, 0, 0, 0	},
-	{ "\pLook Up",		 	kIconSuiteID_LookRight, 	0, kISpElementKind_Button,		kISpElementLabel_Btn_LookUp,		kISpNeedFlag_Button_AlreadyAxis | kISpNeedFlag_Button_ActiveWhenDown, 0, 0, 0	},
-	{ "\pLook Down",	 	kIconSuiteID_LookRight, 	0, kISpElementKind_Button,		kISpElementLabel_Btn_LookDown,		kISpNeedFlag_Button_AlreadyAxis | kISpNeedFlag_Button_ActiveWhenDown, 0, 0, 0	},
-	{ "\pLook Ahead",	 	kIconSuiteID_LookRight, 	0, kISpElementKind_Button,		kISpElementLabel_None,				kISpNeedFlag_Button_AlreadyAxis , 0, 0, 0	},
-	{ "\pPrevious Weapon", 	kIconSuiteID_LookRight, 	0, kISpElementKind_Button,		kISpElementLabel_Btn_Previous,		kISpNeedFlag_Button_AlreadyAxis , 0, 0, 0	},
-	{ "\pNext Weapon",	 	kIconSuiteID_LookRight, 	0, kISpElementKind_Button,		kISpElementLabel_Btn_Next,			kISpNeedFlag_Button_AlreadyAxis , 0, 0, 0	},
-	{ "\pAction", 			kIconSuiteID_LookRight, 	0, kISpElementKind_Button,		kISpElementLabel_Btn_Select,		kISpNeedFlag_Button_AlreadyAxis , 0, 0, 0	},
-	{ "\pAuto Map", 		kIconSuiteID_LookRight, 	0, kISpElementKind_Button,		kISpElementLabel_None,				kISpNeedFlag_Button_AlreadyAxis , 0, 0, 0	},
-	{ "\pMicrophone",	 	kIconSuiteID_LookRight, 	0, kISpElementKind_Button,		kISpElementLabel_None,				kISpNeedFlag_Button_AlreadyAxis , 0, 0, 0	},
-	{ "\pQuit",				kIconSuiteID_ZThrust, 		0, kISpElementKind_Button,		kISpElementLabel_Btn_PauseResume,	kISpNeedFlag_Utility , 0, 0, 0	},
-
+	{ "\pFire Primary", 	Icon_FirePrimary,		0, kISpElementKind_Button,		kISpElementLabel_Btn_Fire,			kISpNeedFlag_Button_AlreadyAxis | kISpNeedFlag_Button_ActiveWhenDown, 0, 0, 0	},
+	{ "\pFire Secondary", 	Icon_FireSecondary,		0, kISpElementKind_Button,		kISpElementLabel_Btn_SecondaryFire,	kISpNeedFlag_Button_AlreadyAxis | kISpNeedFlag_Button_ActiveWhenDown, 0, 0, 0	},
+	{ "\pMove Forward",		Icon_MoveForward, 		0, kISpElementKind_Button,		kISpElementLabel_Btn_MoveForward,	kISpNeedFlag_Button_AlreadyAxis | kISpNeedFlag_Button_ActiveWhenDown, 0, 0, 0	},
+	{ "\pMove Backward",	Icon_MoveBackward, 		0, kISpElementKind_Button,		kISpElementLabel_Btn_MoveBackward,	kISpNeedFlag_Button_AlreadyAxis | kISpNeedFlag_Button_ActiveWhenDown, 0, 0, 0	},
+	{ "\pLook Left", 		Icon_TurnLeft,			0, kISpElementKind_Button,		kISpElementLabel_Btn_TurnLeft,		kISpNeedFlag_Button_AlreadyAxis | kISpNeedFlag_Button_ActiveWhenDown, 0, 0, 0	},
+	{ "\pLook Right", 		Icon_TurnRight, 		0, kISpElementKind_Button,		kISpElementLabel_Btn_TurnRight,		kISpNeedFlag_Button_AlreadyAxis | kISpNeedFlag_Button_ActiveWhenDown, 0, 0, 0	},
+	{ "\pSidestep Left", 	Icon_MoveLeft, 			0, kISpElementKind_Button,		kISpElementLabel_Btn_SlideLeft,		kISpNeedFlag_Button_AlreadyAxis | kISpNeedFlag_Button_ActiveWhenDown, 0, 0, 0	},
+	{ "\pSidestep Right", 	Icon_MoveRight,		 	0, kISpElementKind_Button,		kISpElementLabel_Btn_SlideRight,	kISpNeedFlag_Button_AlreadyAxis | kISpNeedFlag_Button_ActiveWhenDown, 0, 0, 0	},
+	{ "\pGlance Left",	 	Icon_LookLeft, 			0, kISpElementKind_Button,		kISpElementLabel_Btn_LookLeft,		kISpNeedFlag_Button_AlreadyAxis | kISpNeedFlag_Button_ActiveWhenDown, 0, 0, 0	},
+	{ "\pGlance Right", 	Icon_LookRight, 		0, kISpElementKind_Button,		kISpElementLabel_Btn_LookRight,		kISpNeedFlag_Button_AlreadyAxis | kISpNeedFlag_Button_ActiveWhenDown, 0, 0, 0	},
+	{ "\pLook Up",		 	Icon_LookUp, 			0, kISpElementKind_Button,		kISpElementLabel_Btn_LookUp,		kISpNeedFlag_Button_AlreadyAxis | kISpNeedFlag_Button_ActiveWhenDown, 0, 0, 0	},
+	{ "\pLook Down",	 	Icon_LookDown, 			0, kISpElementKind_Button,		kISpElementLabel_Btn_LookDown,		kISpNeedFlag_Button_AlreadyAxis | kISpNeedFlag_Button_ActiveWhenDown, 0, 0, 0	},
+	{ "\pLook Ahead",	 	Icon_LookForward,	 	0, kISpElementKind_Button,		kISpElementLabel_None,				kISpNeedFlag_Button_AlreadyAxis , 0, 0, 0	},
+	{ "\pPrevious Weapon", 	Icon_PrevWeapon, 		0, kISpElementKind_Button,		kISpElementLabel_Btn_Previous,		kISpNeedFlag_Button_AlreadyAxis , 0, 0, 0	},
+	{ "\pNext Weapon",	 	Icon_NextWeapon, 		0, kISpElementKind_Button,		kISpElementLabel_Btn_Next,			kISpNeedFlag_Button_AlreadyAxis , 0, 0, 0	},
+	{ "\pAction", 			Icon_Action, 			0, kISpElementKind_Button,		kISpElementLabel_Btn_Select,		kISpNeedFlag_Button_AlreadyAxis , 0, 0, 0	},
+	{ "\pRun/Walk Toggle",	Icon_RunNotWalk,		0, kISpElementKind_Button,		kISpElementLabel_Btn_Run,			kISpNeedFlag_Button_AlreadyAxis , 0, 0, 0	},
+	{ "\pMove Sideways",	Icon_SideNotTurn,		0, kISpElementKind_Button,		kISpElementLabel_Btn_SideStep,		kISpNeedFlag_Button_AlreadyAxis , 0, 0, 0	},
+	{ "\pLook Sideways",	Icon_LookNotTurn,		0, kISpElementKind_Button,		kISpElementLabel_Btn_Look,			kISpNeedFlag_Button_AlreadyAxis , 0, 0, 0	},
+	{ "\pAuto Map", 		Icon_AutoMap, 			0, kISpElementKind_Button,		kISpElementLabel_None,				kISpNeedFlag_Button_AlreadyAxis , 0, 0, 0	},
+	{ "\pMicrophone",	 	Icon_Microphone,		0, kISpElementKind_Button,		kISpElementLabel_None,				kISpNeedFlag_Button_AlreadyAxis , 0, 0, 0	},
+	{ "\pQuit",				Icon_Quit, 				0, kISpElementKind_Button,		kISpElementLabel_Btn_PauseResume,	kISpNeedFlag_Utility , 0, 0, 0	},
+	
 	// Standard Delta Items
-	{ "\pLook Horizontal",	kIconSuiteID_LookRight, 	0, kISpElementKind_Delta,		kISpElementLabel_Delta_Yaw,			kISpNeedFlag_Delta_AlreadyButton , 0, 0, 0	},
-	{ "\pLook Vertical",	kIconSuiteID_LookRight, 	0, kISpElementKind_Delta,		kISpElementLabel_Delta_Pitch,		kISpNeedFlag_Delta_AlreadyButton , 0, 0, 0	},
-	{ "\pMove",				kIconSuiteID_LookRight, 	0, kISpElementKind_Delta,		kISpElementLabel_Delta_Z,			kISpNeedFlag_Delta_AlreadyButton , 0, 0, 0	},
+	{ "\pLook Horizontal",	Icon_LookHorizontal,	0, kISpElementKind_Delta,		kISpElementLabel_Delta_Yaw,			kISpNeedFlag_Delta_AlreadyButton , 0, 0, 0	},
+	{ "\pLook Vertical",	Icon_LookVertical,		0, kISpElementKind_Delta,		kISpElementLabel_Delta_Pitch,		kISpNeedFlag_Delta_AlreadyButton , 0, 0, 0	},
+	{ "\pMove",				Icon_Move, 				0, kISpElementKind_Delta,		kISpElementLabel_Delta_Z,			kISpNeedFlag_Delta_AlreadyButton , 0, 0, 0	},
 
 	//  Utitilty Class Items
 	/*
@@ -207,6 +245,7 @@ long InputSprocketTestElements(void)
 	err = ISpTickle();
 	assert(err == noErr);
 
+/*
 //Special Cases	
 	assert(gVirtualElements[kBtnQuit]);
 	err = ISpElement_GetSimpleState(gVirtualElements[kBtnQuit], &data);
@@ -216,9 +255,11 @@ long InputSprocketTestElements(void)
 		Stop_ISp();
 		return 0;
 	}
+*/
 	
-//Check each of the action keys
-	for(int a = kBtnFirePrimary; a < kBtnMicrophone; a++)
+// Check each of the action keys
+// LP: These are all *global* actions, ones that are transmitted across the network
+	for(int a = Button_FirstGlobal; a <= Button_LastGlobal; a++)
 	{
 		assert(gVirtualElements[a]);
 		err = ISpElement_GetSimpleState(gVirtualElements[a], &data);
--- Source_Files/Misc/ISp_Support.h
+++ Source_Files/Misc/ISp_Support.h
@@ -1,12 +1,18 @@
 #ifndef __ISp_Support_File
-	#define __ISp_Support_File
+#define __ISp_Support_File
+/*
+	Originally written by someone else
+	
+July 29, 2000 (Loren Petrich):
+	Rewrote it a tiny bit.
+*/
 
-	//Public Function Prototypes
-	void initialize_ISp(void);
-	void ShutDown_ISp(void);
-	void Start_ISp(void);
-	void Stop_ISp(void);
-	long InputSprocketTestElements(void);
-	void ConfigureMarathonISpControls(void);
+// Public Function Prototypes
+void initialize_ISp(void);
+void ShutDown_ISp(void);
+void Start_ISp(void);
+void Stop_ISp(void);
+long InputSprocketTestElements(void);
+void ConfigureMarathonISpControls(void);
 
 #endif
