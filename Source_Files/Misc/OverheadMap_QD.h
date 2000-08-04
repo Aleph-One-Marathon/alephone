9dee2c51903b82c7aa9dcf31b7e4cea348136b38
lpetrich <lpetrich@users.sourceforge.net>
2000-08-04 06:40:58 +0000

    Quickdraw subclass header for overhead-map renderer
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@115 a75d40c9-0969-4759-85e5-97cb871b7ed4

---
+++ Source_Files/Misc/OverheadMap_QD.h
@@ -0,0 +1,55 @@
+#ifndef _OVERHEAD_MAP_QUICKDRAW_CLASS_
+#define _OVERHEAD_MAP_QUICKDRAW_CLASS_
+/*
+	
+	Overhead-Map Quickdraw Class
+	by Loren Petrich,
+	August 3, 2000
+	
+	Subclass of OverheadMapClass for doing rendering with Classic MacOS Quickdraw
+*/
+
+#include "OverheadMapRenderer.h"
+
+class OverheadMap_QD_Class: public OverheadMapClass
+{
+	void draw_polygon(
+		short vertex_count,
+		short *vertices,
+		rgb_color& color);
+
+	void draw_line(
+		short *vertices,
+		rgb_color& color,
+		short pen_size);
+
+	void draw_thing(
+		world_point2d& center,
+		rgb_color& color,
+		short shape,
+		short radius);
+	
+	void draw_player(
+		world_point2d& center,
+		angle facing,
+		rgb_color& color,
+		short shrink,
+		short front,
+		short rear,
+		short rear_theta);
+	
+	void draw_text(
+		world_point2d& location,
+		rgb_color& color,
+		char *text,
+		FontDataStruct& FontData,
+		short which,
+		short justify);
+	
+	void set_path_drawing(rgb_color& color);
+	void draw_path(
+		short step,	// 0: first point
+		world_point2d& location);
+};
+
+#endif
