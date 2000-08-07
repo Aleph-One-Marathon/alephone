d5a093ad0d62a39bc088bb6999c37fff52303859
lpetrich <lpetrich@users.sourceforge.net>
2000-08-07 02:17:51 +0000

    OpenGL subclass code for overhead-map renderer
    Added perimeter drawing for the player symbol -- should be more visible when small
    
    
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@128 a75d40c9-0969-4759-85e5-97cb871b7ed4

--- Source_Files/Misc/OverheadMap_OGL.cpp
+++ Source_Files/Misc/OverheadMap_OGL.cpp
@@ -31,6 +31,10 @@ Jul 17, 2000:
 		that has yielded a significant performance improvement.
 	Same for the polygons, but with relatively little improvement.
 [End notes]
+
+Aug 6, 2000 (Loren Petrich):
+	Added perimeter drawing to drawing commands for the player object;
+	this guarantees that this object will always be drawn reasonably correctly
 */
 
 #include "OverheadMap_OGL.h"
@@ -277,6 +281,7 @@ void OverheadMap_OGL_Class::draw_player(
 	glScalef(scale,scale,1);
 	glVertexPointer(2,GL_FLOAT,0,PlayerShape[0]);
 	glDrawArrays(GL_POLYGON,0,3);
+	glDrawArrays(GL_LINE_LOOP,0,3);	// LP addition: perimeter drawing makes small version easier to see
 	glPopMatrix();
 }
 
