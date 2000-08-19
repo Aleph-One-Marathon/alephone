2fcf5ad1d5eecd989a0474227842819d0a9990fa
cbauer <cbauer@users.sourceforge.net>
2000-08-19 11:05:29 +0000

    - added file containing dummy networking routines to aid in porting
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@179 a75d40c9-0969-4759-85e5-97cb871b7ed4

---
+++ Source_Files/Network/network_dummy.cpp
@@ -0,0 +1,62 @@
+/*
+ *  network_dummy.cpp - Dummy network functions
+ */
+
+#include "cseries.h"
+#include "map.h"
+#include "network.h"
+#include "network_games.h"
+
+
+void NetExit(void)
+{
+}
+
+boolean NetSync(void)
+{
+	return true;
+}
+
+boolean NetUnSync(void)
+{
+	return true;
+}
+
+short NetGetLocalPlayerIndex(void)
+{
+	return 0;
+}
+
+short NetGetPlayerIdentifier(short player_index)
+{
+	return 0;
+}
+
+short NetGetNumberOfPlayers(void)
+{
+	return 1;
+}
+
+void *NetGetPlayerData(short player_index)
+{
+	return NULL;
+}
+
+void *NetGetGameData(void)
+{
+	return NULL;
+}
+
+boolean NetChangeMap(struct entry_point *entry)
+{
+	return false;
+}
+
+long NetGetNetTime(void)
+{
+	return 0;
+}
+
+void display_net_game_stats(void)
+{
+}
