a04597d659180c7f47a452f74eacfa5dd7667069
lpetrich <lpetrich@users.sourceforge.net>
2000-08-24 04:27:26 +0000

    Abstract base class for handling various tasks associated with files
    Modified it to be a concrete one, and one with two classes (FileObject and OpenedFile) -- and at Christian Bauer's suggestion, put the MacOS-specific stuff in it, to be selected with preprocessor statements.
    
    
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@205 a75d40c9-0969-4759-85e5-97cb871b7ed4

--- Source_Files/Misc/FileHandler.h
+++ Source_Files/Misc/FileHandler.h
@@ -1,7 +1,6 @@
 #ifndef _FILE_HANDLER_
 #define _FILE_HANDLER_
 /*
-	
 	File-handler classes
 	by Loren Petrich,
 	August 11, 2000
@@ -13,26 +12,45 @@
 	Attempted to support stdio I/O directly, but on my Macintosh, at least,
 	the performance was much poorer. This is possibly due to "fseek" having to
 	actually read the file or something.
+	
+	Merged all the Macintosh-specific code into these base classes, so that
+	it will be selected with a preprocessor statement when more than one file-I/O
+	API is supported.
 */
 
+// Symbolic constant for a closed file's reference number (refnum)
+const short RefNum_Closed = -1;
 
 /*
 	Abstraction for opened files; it does reading, writing, and closing of such files,
 	without doing anything to the files' specifications
 */
-struct OpenedFile
+class OpenedFile
 {
-	virtual bool IsOpen()=0;
-	virtual bool Close()=0;
+	// This class will need to set the refnum and error value appropriately 
+	friend class FileSpecifier;
 	
-	virtual bool GetPosition(long& Position)=0;
-	virtual bool SetPosition(long Position)=0;
+	// MacOS-specific variables:
+	short RefNum;	// File reference number
+	OSErr Err;		// Error code
 	
-	virtual bool GetLength(long& Length)=0;
-	virtual bool SetLength(long Length)=0;
+public:
 	
-	virtual bool Read(long Count, void *Buffer)=0;
-	virtual bool Write(long Count, void *Buffer)=0;
+	// MacOS-specific
+	short GetRefNum() {return RefNum;}
+	OSErr GetError() {return Err;}
+	
+	bool IsOpen() {return (RefNum != RefNum_Closed);}	
+	bool Close();
+	
+	bool GetPosition(long& Position);
+	bool SetPosition(long Position);
+	
+	bool GetLength(long& Length);
+	bool SetLength(long Length);
+	
+	bool Read(long Count, void *Buffer);
+	bool Write(long Count, void *Buffer);
 	
 	// Smart macros for more easy reading of structured objects
 	
@@ -47,11 +65,105 @@ struct OpenedFile
 	
 	template<class T> bool WriteObjectList(int NumObjects, T* ObjectList)
 		{return Write(NumObjects*sizeof(T),ObjectList);}
+	
+	// Set the file to initially closed
+	OpenedFile(): RefNum(RefNum_Closed), Err(noErr) {}
+	
+	// Auto-close when destroying
+	~OpenedFile() {Close();}
+};
+
+/*
+	Abstraction for loaded resources;
+	this object will release that resource when it finishes.
+*/
+class LoadedResource
+{
+	// This class grabs a resource
+	friend class OpenedResourceFile;
+	
+	// MacOS-specific:
+	Handle RsrcHandle;
+	
+	// Detaches an allocated handle from this object
+	// (keep private to avoid memory leaks)
+	void Detach()
+		{if (RsrcHandle)
+			{HUnlock(RsrcHandle); RsrcHandle = NULL;}}
+public:
+	
+	// Handle loaded?
+	bool IsLoaded() {return (RsrcHandle != NULL);}
+	
+	// Unloads an allocated handle
+	void Unload()
+		{if (RsrcHandle)
+			{HUnlock(RsrcHandle); ReleaseResource(RsrcHandle); RsrcHandle = NULL;}}
+	
+	void *GetPointer(bool DoDetach = false)
+		{
+		if (RsrcHandle)
+			{return *RsrcHandle; if (DoDetach) Detach();}
+		else
+			return NULL;
+		}
+	Handle GetHandle(bool DoDetach = false)
+		{return RsrcHandle; if (DoDetach) Detach();}
+
+	LoadedResource(): RsrcHandle(NULL) {}
+	~LoadedResource() {Unload();}
 };
 
 
-struct FileObject
+/*
+	Abstraction for opened resource files:
+	it does opening, setting, and closing of such files;
+	also getting "LoadedResource" objects that return pointers
+*/
+class OpenedResourceFile
 {
+	// This class will need to set the refnum and error value appropriately 
+	friend class FileSpecifier;
+	
+	// MacOS-specific variables:
+	short RefNum;	// File reference number
+	OSErr Err;		// Error code
+	
+	short SavedRefNum;
+public:
+	
+	// MacOS-specific
+	short GetRefNum() {return RefNum;}
+	OSErr GetError() {return Err;}
+
+	// Pushing and popping the current file -- necessary in the MacOS version,
+	// since resource forks are globally open with one of them the current top one.
+	// Push() saves the earlier top one makes the current one the top one,
+	// while Pop() restores the earlier top one.
+	bool Push();
+	bool Pop();
+
+	// Pushing and popping are unnecessary for the MacOS version of Get()
+	bool Get(OSType Type, short ID, LoadedResource& Rsrc);
+
+	bool IsOpen() {return (RefNum != RefNum_Closed);}
+	bool Close();
+	
+	// Set the file to initially closed
+	OpenedResourceFile(): RefNum(RefNum_Closed), Err(noErr), SavedRefNum(RefNum_Closed) {}
+	
+	// Auto-close when destroying
+	~OpenedResourceFile() {Close();}
+};
+
+
+struct FileSpecifier
+{	
+	FSSpec Spec;
+	OSErr Err;
+
+public:
+
 	// Possible typecodes:
 	// NONE means ignore typecode info
 	enum
@@ -64,78 +176,68 @@ struct FileObject
 		C_Shape,	// For shapes
 		C_Sound,	// For sounds
 		C_Patch,	// For patches
+		C_Images,	// For images
+		C_Prefs,	// For preferences
 		C_NUMBER_OF_TYPECODES
 	};
+
+	
+	// Filespec management
+	void SetSpec(FSSpec& _Spec);
+	FSSpec& GetSpec() {return Spec;}
+
+	// The error:
+	OSErr GetError() {return Err;}
 	
 	// The name as a C string:
 	// assumes enough space to hold it if getting (max. 256 bytes)
 	// The typecode is for automatically adding a suffix;
 	// NONE means add none
 	
-	virtual void GetName(char *Name)=0;
-	virtual void SetName(char *Name, int Type)=0;
+	void GetName(char *Name);
+	void SetName(char *Name, int Type);
 	
 	// Parent directories:
 		
-	virtual bool SetFileToApp()=0;
-	virtual bool SetParentToPreferences()=0;
+	bool SetFileToApp();
+	bool SetParentToPreferences();
 	
 	// Partially inspired by portable_files.h:
 	
-	// The typecode is for adding it as a file attribute
-	virtual bool Create(int Type)=0;
-	virtual bool Open(bool Writable=false)=0;
-
-	virtual bool Open(OpenedFile& OFile, bool Writable=false)=0;
+	// These functions take an appropriate one of the typecodes used earlier;
+	// this is to try to cover the cases of both typecode attributes
+	// and typecode suffixes.
+	bool Create(int Type);
+	// bool Open(bool Writable = false);
+	
+	// Opens a file:
+	bool Open(OpenedFile& OFile, bool Writable=false);
+	
+	// Opens either a MacOS resource fork or some imitation of it:
+	bool Open(OpenedResourceFile& OFile, bool Writable=false);
 	
 	// These calls are for creating dialog boxes to set the filespec
 	// A null pointer means an empty string
-	// The typecode is for restricting the display
-	virtual bool ReadDialog(int Type, char *Prompt=NULL)=0;
-	virtual bool WriteDialog(int Type, char *Prompt=NULL, char *DefaultName=NULL)=0;
+	bool ReadDialog(int Type, char *Prompt=NULL);
+	bool WriteDialog(int Type, char *Prompt=NULL, char *DefaultName=NULL);
 	
-	// Check on whether a file exists, is open, and its type
-	virtual bool Exists()=0;
-	virtual bool IsOpen()=0;
+	// Check on whether a file exists, and its type
+	bool Exists();
 	
 	// Returns NONE if the type could not be identified
-	virtual int GetType()=0;
-	bool IsTypeCorrect(int Type) {return (Type == GetType());}
-	
-	virtual bool Close()=0;
-	
-	virtual bool GetPosition(long& Position)=0;
-	virtual bool SetPosition(long Position)=0;
-	
-	virtual bool GetLength(long& Length)=0;
-	virtual bool SetLength(long Length)=0;
-	
-	virtual bool Read(long Count, void *Buffer)=0;
-	virtual bool Write(long Count, void *Buffer)=0;
+	int GetType();
 	
 	// How many bytes are free in the disk that the file lives in?
-	virtual bool GetFreeSpace(unsigned long& FreeSpace)=0;
+	bool GetFreeSpace(unsigned long& FreeSpace);
 	
 	// Copying: either copy the filespec or copy the whole file
 	// into the current file object
-	virtual bool CopySpec(FileObject& File)=0;
-	virtual bool CopyContents(FileObject& File)=0;
+	bool CopySpec(FileSpecifier& File);
+	bool CopyContents(FileSpecifier& File);
 	
-	// Smart macros for more easy reading of structured objects
+	bool Delete();
 	
-	template<class T> bool ReadObject(T& Object)
-		{return Read(sizeof(T),&Object);}
-	
-	template<class T> bool ReadObjectList(int NumObjects, T* ObjectList)
-		{return Read(NumObjects*sizeof(T),ObjectList);}
-	
-	template<class T> bool WriteObject(T& Object)
-		{return Write(sizeof(T),&Object);}
-	
-	template<class T> bool WriteObjectList(int NumObjects, T* ObjectList)
-		{return Write(NumObjects*sizeof(T),ObjectList);}
-	
-	virtual bool Delete()=0;
+	FileSpecifier(): Err(noErr) {}
 };
 
 
