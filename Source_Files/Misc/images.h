89b713b3e676a5620c6fbc44ecadffa111fb4871
lpetrich <lpetrich@users.sourceforge.net>
2000-08-24 04:28:05 +0000

    Abstraction of file code
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@207 a75d40c9-0969-4759-85e5-97cb871b7ed4

--- Source_Files/Misc/images.h
+++ Source_Files/Misc/images.h
@@ -5,6 +5,10 @@
 
 	images.h
 	Thursday, July 20, 1995 3:30:51 PM- rdm created.
+	
+
+Aug 21, 2000 (Loren Petrich)
+	Added file-object support
 
 */
 
@@ -16,19 +20,30 @@ boolean scenario_picture_exists(short base_resource);
 struct color_table *calculate_picture_clut(short pict_resource_number);
 struct color_table *build_8bit_system_color_table(void);
 
-void set_scenario_images_file(FileDesc *file);
+void set_scenario_images_file(FileSpecifier& File);
+// void set_scenario_images_file(FileDesc *file);
 
 void draw_full_screen_pict_resource_from_images(short pict_resource_number);
 void draw_full_screen_pict_resource_from_scenario(short pict_resource_number);
 
 void scroll_full_screen_pict_resource_from_scenario(short pict_resource_number, boolean text_block);
 
+// Places a MacOS resource handle into an appropriate wrapper object;
+// a resource-fork emulator may put a pointer instead.
+bool get_picture_resource_from_images(short base_resource, LoadedResource& PictRsrc);
+bool get_picture_resource_from_scenario(short base_resource, LoadedResource& PictRsrc);
+
+bool get_sound_resource_from_scenario(short resource_number, LoadedResource& SndRsrc);
+
+
+/*
 #ifdef mac
 PicHandle get_picture_resource_from_images(short base_resource);
 PicHandle get_picture_resource_from_scenario(short base_resource);
 
 SndListHandle get_sound_resource_from_scenario(short resource_number);
 #endif
+*/
 
 #endif
 
