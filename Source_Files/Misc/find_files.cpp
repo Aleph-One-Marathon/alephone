94ab12f0470700dbfd5977f3d8f3436427d3db8e
lpetrich <lpetrich@users.sourceforge.net>
2000-08-27 13:24:37 +0000

    Partially converted to file-object abstractions
    Now the implementation of a file-finder object
    
    Got rid of some sizeof's with some csmacros.h template wrappers for memcpy and memset
    
    
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@268 a75d40c9-0969-4759-85e5-97cb871b7ed4

--- Source_Files/Misc/find_files.cpp
+++ Source_Files/Misc/find_files.cpp
@@ -31,7 +31,7 @@ Aug 28, 2000 (Loren Petrich):
 
 void FileFinder::Clear()
 {
-	memset(this,0,sizeof(FileFinder));
+	obj_clear(*this);
 }
 
 
@@ -91,7 +91,7 @@ bool FileFinder::Enumerate(DirectorySpecifier& Dir)
 	// Local variable
 	short index;
 
-	memset(&pb, 0, sizeof(CInfoPBRec));
+	obj_clear(pb);
 	
 	pb.hFileInfo.ioVRefNum= Dir.Get_vRefNum();
 	pb.hFileInfo.ioNamePtr= temp_file.name;
@@ -174,129 +174,3 @@ static int alphabetical_names(
 	return (IUCompString(((FSSpec const *)a)->name, ((FSSpec const *)b)->name)); 
 }
 */
-
-// Original code, for reference purposes
-#if 0
-/* ---------------- Parameter Block Version */
-OSErr find_files(
-	struct find_file_pb *param_block)
-{
-	OSErr err;
-
-	/* If we need to create the destination buffer */
-	if(param_block->flags & _ff_create_buffer) 
-	{
-		assert(param_block->search_type==_fill_buffer);
-		param_block->buffer= (FSSpec *) NewPtr(sizeof(FSSpec)*param_block->max);
-	}
-
-	/* Assert that we got it. */
-	assert(param_block->search_type==_callback_only || param_block->buffer);
-	assert(param_block->version==0);
-
-	/* Set the variables */
-	param_block->count= 0;
-
-	err= enumerate_files(param_block, param_block->BaseDir.Get_parID());
-	// err= enumerate_files(param_block, param_block->directory_id);
-
-	/* Alphabetical */
-	if(param_block->flags & _ff_alphabetical)
-	{
-		assert(param_block->search_type==_fill_buffer);
-		qsort(param_block->buffer, param_block->count, 
-			sizeof(FSSpec), alphabetical_names);
-	}
-
-	/* If we created the buffer, make it the exact right size */
-	if(param_block->flags & _ff_create_buffer)
-	{
-		assert(param_block->search_type==_fill_buffer);
-		SetPtrSize((Ptr) param_block->buffer, sizeof(FSSpec)*(param_block->count));
-	}
-	
-	return err;
-}
-
-
-/* --------- Local Code --------- */
-static OSErr enumerate_files(
-	struct find_file_pb *param_block, 
-	long directory_id) /* Because it is recursive.. */
-{
-	static CInfoPBRec pb; /* static to prevent stack overflow.. */
-	// Kludge to make the FSSpec always available
-	static FileSpecifier TempFile;
-	static FSSpec& temp_file = TempFile.GetSpec();
-	static OSErr err;
-	short index;
-
-	memset(&pb, 0, sizeof(CInfoPBRec));
-	
-	temp_file.vRefNum= param_block->BaseDir.Get_vRefNum();
-	// temp_file.vRefNum= param_block->vRefNum;
-	pb.hFileInfo.ioVRefNum= temp_file.vRefNum;
-	pb.hFileInfo.ioNamePtr= temp_file.name;
-			
-	for(err= noErr, index=1; param_block->count<param_block->max && err==noErr; index++) 
-	{
-		pb.hFileInfo.ioDirID= directory_id;
-		pb.hFileInfo.ioFDirIndex= index;
-
-		err= PBGetCatInfo( &pb, false);
-		if(!err)
-		{
-			if ((pb.hFileInfo.ioFlAttrib & 16) && (param_block->flags & _ff_recurse))
-			{
-				/* Recurse, if you really want to... */
-				err= enumerate_files(param_block, pb.dirInfo.ioDrDirID);
-			} else {
-				/* Add.. */
-				if(param_block->type_to_find==WILDCARD_TYPE || 
-					pb.hFileInfo.ioFlFndrInfo.fdType==param_block->type_to_find)
-				{
-					temp_file.vRefNum= pb.hFileInfo.ioVRefNum;
-					temp_file.parID= directory_id;
-					
-					/* Only add if there isn't a callback or it returns TRUE */
-					switch(param_block->search_type)
-					{
-						case _fill_buffer:
-							if(!param_block->callback || param_block->callback(TempFile, param_block->user_data))
-							// if(!param_block->callback || param_block->callback(&temp_file, param_block->user_data))
-							{
-								/* Copy it in.. */
-								BlockMove(&temp_file, &param_block->buffer[param_block->count++], 
-									sizeof(FSSpec));
-							}
-							break;
-							
-						case _callback_only:
-							assert(param_block->callback);
-							if(param_block->flags & _ff_callback_with_catinfo)
-							{
-								param_block->callback(TempFile, &pb);
-								// param_block->callback(&temp_file, &pb);
-							} else {
-								param_block->callback(TempFile, param_block->user_data);
-								// param_block->callback(&temp_file, param_block->user_data);
-							}
-							break;
-							
-						default:
-							// LP change:
-							assert(false);
-							// halt();
-							break;
-					}
-				}
-			}
-		} else {
-			/* We ran out of files.. */
-		}
-	}
-
-	/* If we got a fnfErr, it was because we indexed too far. */	
-	return (err==fnfErr) ? (noErr) : err;
-}
-#endif
