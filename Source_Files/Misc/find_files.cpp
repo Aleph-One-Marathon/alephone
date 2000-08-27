8b978b6c5aafeac6a96705bcfa59c5502c696edf
lpetrich <lpetrich@users.sourceforge.net>
2000-08-27 05:40:35 +0000

    Partially converted to file-object abstractions
    Now the implementation of a file-finder object
    
    
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@237 a75d40c9-0969-4759-85e5-97cb871b7ed4

--- Source_Files/Misc/find_files.cpp
+++ Source_Files/Misc/find_files.cpp
@@ -8,6 +8,9 @@ Feb. 4, 2000 (Loren Petrich):
 Aug 25, 2000 (Loren Petrich):
 	Abstracting the file handling
 
+Aug 28, 2000 (Loren Petrich):
+	Put these functions into the FileFinder object; eradicated the "param_block->" code, etc.
+	and did other things to abstract it.
 */
 
 #include "macintosh_cseries.h"
@@ -21,9 +24,159 @@ Aug 25, 2000 (Loren Petrich):
 #endif
 
 /* --------- local prototypes */
-static int alphabetical_names(void const *a, void const *b);
-static OSErr enumerate_files(struct find_file_pb *param_block, long directory_id);
+// static int alphabetical_names(void const *a, void const *b);
+// static OSErr enumerate_files(struct find_file_pb *param_block, long directory_id);
+
+// Code:
+
+void FileFinder::Clear()
+{
+	memset(this,0,sizeof(FileFinder));
+}
+
+
+/* ---------------- Parameter Block Version */
+// OSErr find_files(
+//	struct find_file_pb *param_block)
+bool FileFinder::Find()
+{
+	// Translate the type info:
+	type_to_find = (Type != WILDCARD_TYPE) ? get_typecode(Type) : 0;
 	
+	// Eliminated buffer creation as unused
+	
+	/* Assert that we got it. */
+	assert(search_type==_callback_only || buffer);
+	assert(version==0);
+
+	/* Set the variables */
+	count= 0;
+
+	Enumerate(BaseDir);
+	
+	// Eliminated alphabetical order as unused
+		
+	return (Err == noErr);
+}
+
+/*
+Boolean equal_fsspecs(
+	FSSpec *a, 
+	FSSpec *b)
+{
+	Boolean equal= false;
+	
+	if(a->vRefNum==b->vRefNum && a->parID==b->parID && 
+		EqualString(a->name, b->name, false, false))
+	{
+		equal= true;
+	}
+	
+	return equal;
+}
+*/
+
+/* --------- Local Code --------- */
+// static OSErr enumerate_files(
+//	struct find_file_pb *param_block, 
+//	long directory_id) /* Because it is recursive.. */
+bool FileFinder::Enumerate(DirectorySpecifier& Dir)
+{
+	// Set up the temporary file
+	TempFile.FromDirectory(Dir);
+
+	// Kludge to make the FSSpec always available
+	FSSpec& temp_file = TempFile.GetSpec();
+	
+	// Local variable
+	short index;
+
+	memset(&pb, 0, sizeof(CInfoPBRec));
+	
+	pb.hFileInfo.ioVRefNum= Dir.Get_vRefNum();
+	pb.hFileInfo.ioNamePtr= temp_file.name;
+			
+	for(Err= noErr, index=1; count<max && Err==noErr; index++) 
+	{
+		pb.hFileInfo.ioDirID= Dir.Get_parID();
+		pb.hFileInfo.ioFDirIndex= index;
+
+		Err= PBGetCatInfo( &pb, false);
+		if(Err == noErr)
+		{
+			if ((pb.hFileInfo.ioFlAttrib & 16) && (flags & _ff_recurse))
+			{
+				/* Recurse, if you really want to... */
+				
+				// Set up volume info
+				DirectorySpecifier SubDir = Dir;
+				// Change the directory
+				SubDir.Set_parID(pb.dirInfo.ioDrDirID);
+				// Go!
+				Enumerate(SubDir);
+				// Reset the temporary file's directory to what's appropriate here
+				// (it's global to this object)
+				TempFile.FromDirectory(Dir);
+				
+			} else {
+				/* Add.. */
+				if(Type==WILDCARD_TYPE || 
+					pb.hFileInfo.ioFlFndrInfo.fdType==type_to_find)
+				{
+					/* Only add if there isn't a callback or it returns TRUE */
+					switch(search_type)
+					{
+						case _fill_buffer:
+							if(!callback || callback(TempFile, user_data))
+							// if(!callback || callback(&temp_file, user_data))
+							{
+								/* Copy it in.. */
+								buffer[count++] = TempFile;
+							}
+							break;
+							
+						case _callback_only:
+							assert(callback);
+							if(flags & _ff_callback_with_catinfo)
+							{
+								callback(TempFile, &pb);
+								// callback(&temp_file, &pb);
+							} else {
+								callback(TempFile, user_data);
+								// callback(&temp_file, user_data);
+							}
+							break;
+							
+						default:
+							// LP change:
+							assert(false);
+							// halt();
+							break;
+					}
+				}
+			}
+		} else {
+			/* We ran out of files.. */
+		}
+	}
+
+	/* If we got a fnfErr, it was because we indexed too far. */	
+	///return (Err==fnfErr) ? (noErr) : Err;
+	if (Err == fnfErr) Err = noErr;
+	
+	return (Err == noErr);
+}
+/*
+static int alphabetical_names(
+	void const *a, 
+	void const *b)
+{
+	return (IUCompString(((FSSpec const *)a)->name, ((FSSpec const *)b)->name)); 
+}
+*/
+
+// Original code, for reference purposes
+#if 0
 /* ---------------- Parameter Block Version */
 OSErr find_files(
 	struct find_file_pb *param_block)
@@ -65,20 +218,6 @@ OSErr find_files(
 	return err;
 }
 
-Boolean equal_fsspecs(
-	FSSpec *a, 
-	FSSpec *b)
-{
-	Boolean equal= false;
-	
-	if(a->vRefNum==b->vRefNum && a->parID==b->parID && 
-		EqualString(a->name, b->name, false, false))
-	{
-		equal= true;
-	}
-	
-	return equal;
-}
 
 /* --------- Local Code --------- */
 static OSErr enumerate_files(
@@ -160,10 +299,4 @@ static OSErr enumerate_files(
 	/* If we got a fnfErr, it was because we indexed too far. */	
 	return (err==fnfErr) ? (noErr) : err;
 }
-
-static int alphabetical_names(
-	void const *a, 
-	void const *b)
-{
-	return (IUCompString(((FSSpec const *)a)->name, ((FSSpec const *)b)->name)); 
-}
+#endif
