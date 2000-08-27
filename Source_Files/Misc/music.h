0ca5582c36abcda31e5f745cc6d22ca279447c56
lpetrich <lpetrich@users.sourceforge.net>
2000-08-27 05:41:36 +0000

    Added file-handler objects
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@243 a75d40c9-0969-4759-85e5-97cb871b7ed4

--- Source_Files/Misc/music.h
+++ Source_Files/Misc/music.h
@@ -5,14 +5,20 @@
 	music.h
 	Wednesday, July 12, 1995 11:43:21 PM
 	This only allows for one song to be played at a given time.
+
+Aug 24, 2000 (Loren Petrich):
+	Adding object-oriented file handling
 */
+
+#include "FileHandler.h"
 
 enum { // All of our songs.
 	_introduction_song= 0,
 	NUMBER_OF_SONGS
 };
 
-boolean initialize_music_handler(FileSpecifier &song_file);
+boolean initialize_music_handler(FileSpecifier& SongFile);
+// boolean initialize_music_handler(FileDesc *song_file);
 
 void queue_song(short song_index);
 
