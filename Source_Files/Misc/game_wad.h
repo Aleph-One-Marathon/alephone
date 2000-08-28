e30cf19d988fffd53cedc98ee6dea424d24bf9d6
lpetrich <lpetrich@users.sourceforge.net>
2000-08-28 08:49:45 +0000

    Did abstraction of the file code (now complete) Moved getting of default map, physics, shapes, sounds to interface.h
    Reconciliation of LP and CB
    
    
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@318 a75d40c9-0969-4759-85e5-97cb871b7ed4

--- Source_Files/Misc/game_wad.h
+++ Source_Files/Misc/game_wad.h
@@ -17,6 +17,36 @@ Aug 12, 2000 (Loren Petrich):
 
 class FileSpecifier;
 
+#ifdef LP
+/* -------------------- Private or map editor functions */
+// Moved them all into game_wad.c, because only game_wad.c uses them
+#if 0
+void allocate_map_for_counts(short polygon_count, short side_count,
+	short endpoint_count, short line_count, long terminal_data_length);
+void load_points(saved_map_pt *points, short count);
+void load_lines(saved_line *lines, short count);
+void load_sides(saved_side *sides, short count, short version);
+void load_polygons(saved_poly *polys, short count, short version);
+void load_lights(saved_static_light *_lights, short count, short version);
+void load_annotations(saved_annotation *annotations, short count);
+void load_objects(saved_object *map_objects, short count);
+void load_media(struct media_data *media, short count);
+void load_map_info(saved_map_data *map_info);
+void load_ambient_sound_images(struct ambient_sound_image_data *data, short count);
+void load_random_sound_images(struct random_sound_image_data *data, short count);
+void load_terminal_data(byte *data, long length);
+
+/* Used _ONLY_ by game_wad.c internally and precalculate.c. */
+boolean process_map_wad(struct wad_data *wad, boolean restoring_game, short version);
+
+/* Final three calls, must be in this order! */
+void recalculate_redundant_map(void);
+void scan_and_add_platforms(struct static_platform_data *platform_static_data, short count);
+void complete_loading_level(short *map_indexes, short map_index_count, 
+	struct static_platform_data *platform_data, short platform_data_count,
+	struct platform_data *actual_platform_data, short actual_platform_data_count, short version);
+#endif
+#endif
 boolean save_game_file(FileSpecifier& File);
 
 /* -------------- New functions */
