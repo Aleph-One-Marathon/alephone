a91889ee6b5502d8b42d9567778991f6b8ad0888
lpetrich <lpetrich@users.sourceforge.net>
2000-08-29 05:58:33 +0000

    Added annotation that the original author of most of these is unknown, but is probably Bo Lindbergh.
    Added include guards to all the header files.
    
    Placed CB's Mac type declarations inside of cstypes.h, and placed that in front of some other definitions.
    
    Has some template functions for doing memcpy and memset on objects and sets of objects
    
    Modified the obj_copy functions so that "source" can be a constant
    
    Added ()'s to TEST_FLAG implementation, a standard C-preprocessor precaution.
    
    
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/branches/LP_fileio@346 a75d40c9-0969-4759-85e5-97cb871b7ed4

--- Source_Files/CSeries/csmacros.h
+++ Source_Files/CSeries/csmacros.h
@@ -9,6 +9,8 @@ Aug 27, 2000 (Loren Petrich):
 #ifndef _CSERIES_MACROS_
 #define _CSERIES_MACROS_
 
+#include <string.h>
+
 #define FLOOR(value,floor) ((value)<(floor) ? (floor) : (value))
 #define CEILING(value,ceiling) ((value)>(ceiling) ? (ceiling) : (value))
 
@@ -32,7 +34,7 @@ Aug 27, 2000 (Loren Petrich):
 #define SET_FLAG16(flags,bit,value) ((void)((value) ? ((flags)|=FLAG16(bit)) : ((flags)&=~FLAG16(bit))))
 
 // LP addition (Mar 2, 2000): some more generic routines for flags
-#define TEST_FLAG(obj,flag) (obj&flag)
+#define TEST_FLAG(obj,flag) ((obj)&(flag))
 #define SET_FLAG(obj,flag,value) ((void)((value)?((obj)|=(flag)):((obj)&=~(flag))))
 
 /*
