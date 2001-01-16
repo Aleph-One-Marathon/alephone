0fd941c9f35500477570f488b2020ce015e68450
cbauer <cbauer@users.sourceforge.net>
2001-01-16 14:11:50 +0000

    updated autogen.sh script
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@743 a75d40c9-0969-4759-85e5-97cb871b7ed4

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,44 @@
 #! /bin/sh
-aclocal
-autoheader
-autoconf
-automake
-./configure $*
+# Run this to generate all the initial makefiles, etc.
+# This was lifted from the Gimp, and adapted slightly by
+# Christian Bauer.
+
+DIE=0
+
+PROG="Aleph One"
+
+(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+        echo
+        echo "You must have autoconf installed to compile $PROG."
+        echo "Download the appropriate package for your distribution,"
+        echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+        DIE=1
+}
+
+(automake --version) < /dev/null > /dev/null 2>&1 || {
+        echo
+        echo "You must have automake installed to compile $PROG."
+        echo "Get ftp://ftp.gnu.org/pub/gnu/automake-1.4.tar.gz"
+        echo "(or a newer version if it is available)"
+        DIE=1
+}
+
+if test "$DIE" -eq 1; then
+        exit 1
+fi
+
+if test -z "$*"; then
+        echo "I am going to run ./configure with no arguments - if you wish "
+        echo "to pass any to it, please specify them on the $0 command line."
+fi
+
+for dir in .
+do 
+  echo processing $dir
+  (cd $dir; \
+  aclocalinclude="$ACLOCAL_FLAGS"; \
+  aclocal $aclocalinclude; \
+  autoheader; automake -a; autoconf)
+done
+
+./configure "$@"
