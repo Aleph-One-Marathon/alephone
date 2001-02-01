7df2393499d98e0a019e9c4f7752cebf9523e7ff
lpetrich <lpetrich@users.sourceforge.net>
2001-02-01 02:22:14 +0000

    Got rid of some old MacOS-specific load/save dialog-box cruft.
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@769 a75d40c9-0969-4759-85e5-97cb871b7ed4

--- Source_Files/Misc/preprocess_map_mac.cpp
+++ Source_Files/Misc/preprocess_map_mac.cpp
@@ -19,6 +19,9 @@ Aug 12, 2000 (Loren Petrich):
 
 Aug 24, 2000 (Loren Petrich):
 	Moved save-dialog Mac-specific code out to FileHandler
+
+Jan 31, 2001 (Loren Petrich);
+	Delete some old MacOS-specific load-and-save junk.
 */
 
 #include "macintosh_cseries.h"
@@ -63,21 +66,8 @@ enum {
 	iLEVEL_NAME
 };
 
-/* Local globals */
-static Str255 save_level_name;
-static SFReply load_file_reply;
-static PicHandle overhead_pict;
-static bool overhead_pict_valid= false;
-static FSSpec previous_selection; /* FSSpec for convenience-> never used as such */
-
 /* ---- local prototypes */
-static void enumerate_catalog(long dir_id);
 static void add_overhead_thumbnail(void);
-pascal short custom_get_hook(short item, DialogPtr theDialog);
-static pascal Boolean custom_get_filter_proc(DialogPtr theDialog, 
-	EventRecord *theEvent, short *itemHit);
-pascal short custom_put_hook(short item, DialogPtr theDialog, void *user_data);
-static bool confirm_save_choice(FSSpec *file);
 
 
 /* --------- code begins */
@@ -138,7 +128,7 @@ bool choose_saved_game_to_load(FileSpecifier& File)
 	return reply.sfGood;
 	*/
 }
-
+extern bool Tracer;
 bool save_game(
 	void)
 {
@@ -163,53 +153,8 @@ bool save_game(
 	
 	hide_cursor();
 	resume_game();
-
-	return success;
-
-// Keep this stuff around; it will be necessary for implementing "replace" as the default
-// instead of "cancel"
-// LP: begin no-compile
-#if 0
-	Str255 prompt;
-	Str63 file_name;	
-	DlgHookYDUPP dlgHook;
-	StandardFileReply reply;
-	Point top_left= {-1, -1}; /* auto center */
-	bool success= false;
-
-	pause_game();
-	show_cursor();
-	
-	/* Translate the name, and display the dialog */
-	get_current_saved_game_name(file_name);
-	
-	/* Create the UPP's */
-	dlgHook= NewDlgHookYDProc(custom_put_hook);
-	assert(dlgHook);
-
-	/* The drawback of this method-> I don't get a New Folder button. */
-	/* If this is terribly annoying, I will add the Sys7 only code. */
-	CustomPutFile(getpstr(prompt, strPROMPTS, _save_game_prompt), 
-		file_name, &reply, 0, top_left, dlgHook, NULL, NULL, NULL, &reply.sfFile);
-
-	/* Free them... */
-	DisposeRoutineDescriptor((UniversalProcPtr) dlgHook);
-
-	if(reply.sfGood)
-	{
-		/* And save it. */
-		if (save_game_file((FileDesc *) &reply.sfFile))
-		{
-			success= true;
-		}
-	}
-
-	hide_cursor();
-	resume_game();
 	
 	return success;
-// LP: end no-compile
-#endif
 }
 
 void add_finishing_touches_to_save_file(FileSpecifier &File)
@@ -315,254 +260,3 @@ static void add_overhead_thumbnail(
 	
 	return;
 }
-
-// LP: begin no-compile
-#if 0
-/* load_file_reply is valid.. */
-pascal short custom_put_hook(
-	short item, 
-	DialogPtr theDialog,
-	void *user_data)
-{
-	FSSpec *selected_file= (FSSpec *) user_data;
-
-	global_idle_proc();
-
-	if(GetWRefCon((WindowPtr) theDialog)==sfMainDialogRefCon)
-	{
-		if(item==sfItemOpenButton)
-		{
-			if(!confirm_save_choice(selected_file))
-			{
-				item= sfHookNullEvent;
-			}
-		}
-	}
-	
-	return item;
-}
-
-#define REPLACE_H_OFFSET 52
-#define REPLACE_V_OFFSET 52
-
-/* Doesn't actually use it as an FSSpec, just easier */
-/* Return true if we want to pass it through, otherwise return false. */
-static bool confirm_save_choice(
-	FSSpec *file)
-{
-	OSErr err;
-	HFileParam pb;
-	bool pass_through= true;
-	DialogPtr dialog;
-	short item_hit;
-	Rect frame;
-
-	/* Clear! */
-	obj_set(pb);
-	pb.ioNamePtr= file->name;
-	pb.ioVRefNum= file->vRefNum;
-	pb.ioDirID= file->parID;
-	err= PBHGetFInfo((HParmBlkPtr) &pb, false); 
-
-	if(!err)
-	{
-		/* IF we aren't a folder.. */
-		if(!(pb.ioFlAttrib & kFolderBit))
-		{
-			if(pb.ioFlFndrInfo.fdType==SAVE_GAME_TYPE)
-			{
-				/* Get the default dialog's frame.. */
-				get_window_frame(FrontWindow(), &frame);
-
-				/* Slam the ParamText */
-				ParamText(file->name, "\p", "\p", "\p");
-
-				/* Load in the dialog.. */
-				dialog= myGetNewDialog(dlogMY_REPLACE, NULL, (WindowPtr) -1, 0);
-				assert(dialog);
-				
-				/* Move the window to the proper location.. */
-				MoveWindow((WindowPtr) dialog, frame.left+REPLACE_H_OFFSET, 
-					frame.top+REPLACE_V_OFFSET, false);
-
-				/* Show the window. */
-				ShowWindow((WindowPtr) dialog);			
-				do {
-					ModalDialog(get_general_filter_upp(), &item_hit);
-				} while(item_hit > iCANCEL);
-
-				/* Restore and cleanup.. */				
-				ParamText("\p", "\p", "\p", "\p");
-				DisposeDialog(dialog);
-				
-				if(item_hit==iOK) /* replace.. */
-				{
-					/* Want to delete it... */
-					err= FSpDelete(file);
-					/* Pass it on through.. they won't bring up the replace now. */
-				} else {
-					/* They cancelled.. */
-					pass_through= false;
-				}
-			}
-		}
-	}
-	
-	return pass_through;
-}
-
-// LP: end no-compile
-#endif
-
-#ifdef OBSOLETE
-static pascal short custom_get_hook(
-	short item, 
-	DialogPtr theDialog)
-{
-	short file_ref;
-	short itemType;
-	Handle item_handle, resource;
-	Rect bounds;
-	GrafPtr old_port;
-	long parID;
-	OSErr err;
-
-	global_idle_proc();
-
-	parID= LMGetCurDirStore();
-
-	/* IF the selection has changed... */	
-	if(previous_selection.vRefNum != load_file_reply.vRefNum 
-		|| previous_selection.parID != parID 
-		|| previous_selection.name[0]!=load_file_reply.fName[0]
-		|| memcmp(previous_selection.name, load_file_reply.fName, load_file_reply.fName[0]+1))
-	{
-		/* .. save the new current one. .. */
-		previous_selection.vRefNum= load_file_reply.vRefNum;
-		previous_selection.parID= parID;
-		BlockMove(load_file_reply.fName, previous_selection.name, load_file_reply.fName[0]+1);
-
-		/* Pessimism.. */
-		overhead_pict_valid= false;
-		save_level_name[0]= 0;
-		
-		/* ... check the file type.. */	
-		if(load_file_reply.fType==SAVE_GAME_TYPE)
-		{
-			/* .. free memory if necessary... */
-			if(overhead_pict)
-			{
-				/* Free it.. */
-				DisposeHandle((Handle) overhead_pict);
-				overhead_pict= (PicHandle) NULL;
-			}
-
-			/* ... read in the thumbnail... */
-			file_ref= HOpenResFile(load_file_reply.vRefNum, parID, load_file_reply.fName, fsRdPerm);
-			if(file_ref!= -1)
-			{
-				resource= Get1Resource('PICT', THUMBNAIL_ID);
-				if(resource)
-				{
-					/* Copy it.. */
-					overhead_pict= (PicHandle) resource;
-					err= HandToHand((Handle *) &overhead_pict);
-					assert(!err);
-					
-					/* Release it. */
-					ReleaseResource(resource);
-
-					overhead_pict_valid= true;
-				} 
-				
-				resource= Get1Resource('STR ', strSAVE_LEVEL_NAME);
-				if(resource)
-				{
-					BlockMove(*resource, save_level_name, (**resource)+1);
-					ReleaseResource(resource);
-				}
-
-				CloseResFile(file_ref);
-			}
-		} 
-		
-		/* Inval the rectangle.. */
-		GetDialogItem(theDialog, iTHUMBNAIL_RECT, &itemType, &item_handle, &bounds);
-		
-		/* Inval, and let the modal proc draw the picture.. */
-		GetPort(&old_port);
-		SetPort((GrafPtr) theDialog);
-		InvalRect(&bounds);
-		SetPort(old_port);
-	}
-	
-	return item;
-}
-
-static pascal Boolean custom_get_filter_proc(
-	DialogPtr theDialog, 
-	EventRecord *theEvent, 
-	short *itemHit)
-{
-	short itemType;
-	Handle item;
-	Rect bounds, frame, name_bounds;
-	GrafPtr old_port;
-	RgnHandle clip_region;
-	FontInfo info;
-
-	(void)(itemHit);
-
-	if(theEvent->what==updateEvt)
-	{
-		if ((DialogPtr)theEvent->message==theDialog)
-		{
-			GetPort(&old_port);
-			SetPort((GrafPtr) theDialog);
-		
-			GetDialogItem(theDialog, iTHUMBNAIL_RECT, &itemType, &item, &bounds);
-			GetDialogItem(theDialog, iLEVEL_NAME, &itemType, &item, &name_bounds);
-
-			frame= bounds;
-
-			/* Draw a border around it. */
-			InsetRect(&frame, -2, -2);
-			FrameRect(&frame);
-		
-			if(overhead_pict_valid)
-			{
-				short text_x, text_y;
-				short text_length;
-			
-				assert(overhead_pict);
-				clip_region= NewRgn();
-				GetClip(clip_region);
-				ClipRect(&bounds);
-				DrawPicture(overhead_pict, &bounds);
-				SetClip(clip_region);
-
-				/* Center the text in the rectangle */
-				pstrcpy(temporary, save_level_name);
-				text_length = *temporary;
-				TruncText(RECTANGLE_WIDTH(&name_bounds), temporary+1, &text_length, smTruncEnd);
-				*temporary = text_length;
-
-				GetFontInfo(&info);
-				text_y= name_bounds.bottom - info.descent;
-				text_x= name_bounds.left + (RECTANGLE_WIDTH(&name_bounds)-StringWidth(temporary))/2;
-				MoveTo(text_x, text_y);
-				EraseRect(&name_bounds);
-				DrawString(temporary);
-			} else {
-				EraseRect(&bounds);
-				EraseRect(&name_bounds);
-			}
-	
-			/* Return to the old port */
-			SetPort(old_port);
-		}
-	}
-
-	return false;
-}
-#endif
