493c7a023049e6f209784a5c021ab5a9eafc1e49
cbauer <cbauer@users.sourceforge.net>
2001-03-12 19:13:24 +0000

    static HUD picture only gets loaded once
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@820 a75d40c9-0969-4759-85e5-97cb871b7ed4

--- Source_Files/Misc/game_window_sdl.cpp
+++ Source_Files/Misc/game_window_sdl.cpp
@@ -44,20 +44,23 @@ void draw_panels(void)
 	}
 
 	// Draw static HUD picture
-	LoadedResource rsrc;
-	if (get_picture_resource_from_images(INTERFACE_PANEL_BASE, rsrc)) {
-		SDL_Surface *s = picture_to_surface(rsrc);
-		if (s) {
-			SDL_Rect dst_rect = {0, 320, 640, 160};
-			SDL_BlitSurface(s, NULL, HUD_Buffer, &dst_rect);
-			SDL_FreeSurface(s);
-
-			// Add dynamic elements
-			_set_port_to_HUD();
-			update_everything(NONE);
-			_restore_port();
-		}
+	static SDL_Surface *static_hud_pict = NULL;
+	static bool hud_pict_not_found = false;
+	if (static_hud_pict == NULL && !hud_pict_not_found) {
+		LoadedResource rsrc;
+		if (get_picture_resource_from_images(INTERFACE_PANEL_BASE, rsrc))
+			static_hud_pict = picture_to_surface(rsrc);
+		else
+			hud_pict_not_found = true;
+	} else {
+		SDL_Rect dst_rect = {0, 320, 640, 160};
+		SDL_BlitSurface(static_hud_pict, NULL, HUD_Buffer, &dst_rect);
 	}
+
+	// Add dynamic elements
+	_set_port_to_HUD();
+	update_everything(NONE);
+	_restore_port();
 
 	// Tell main loop to render the HUD in the next run
 	RequestDrawingHUD();
