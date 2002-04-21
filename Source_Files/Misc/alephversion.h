4e548dbbab5b5e0bfb9fc3c9bbb6b338f7b34175
no author <>
2002-04-21 20:45:17 +0000

    This commit was manufactured by cvs2svn to create branch 'aleph-inio-branch'.
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/branches/aleph-inio-branch@1305 a75d40c9-0969-4759-85e5-97cb871b7ed4

---
+++ Source_Files/Misc/alephversion.h
@@ -0,0 +1,19 @@
+#ifndef __ALEPHVERSION_H
+#define __ALEPHVERSION_H
+
+	#define MAJOR_VERSION   1
+	#define MAJOR_VERSION_S "1"
+	#define MINOR_VERSION   0
+	#define MINOR_VERSION_S "0"
+	#define STEPPING        "d"
+	#define BUILDNUM        1
+	#define BUILDSTR        "1"
+	#define BRANCH          "(inio)"
+	#define FINAL	        0
+	
+	#if FINAL
+		#define VERSION_STRING MAJOR_VERSION_S "." MINOR_VERSION_S STEPPING BUILDSTR BRANCH
+	#else
+		#define VERSION_STRING MAJOR_VERSION_S "." MINOR_VERSION_S STEPPING BUILDSTR "pre" BRANCH "(" __DATE__ " " __TIME__ ")"
+	#endif
+#endif
