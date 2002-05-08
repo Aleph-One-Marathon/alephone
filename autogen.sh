8163d3a3ff3609afcfa5b9e55df78b5f4e8e6fa4
astrange <astrange@users.sourceforge.net>
2002-05-08 21:48:15 +0000

    New and improved
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@1327 a75d40c9-0969-4759-85e5-97cb871b7ed4

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,18 @@
-#! /bin/sh
+#!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 # This was lifted from the Gimp, and adapted slightly by
-# Christian Bauer.
+# Raph Levien, slightly hacked for xine by Daniel Caujolle-Bert.
 
 DIE=0
 
-PROG="Aleph One"
+PROG=liboss
+
+# Check how echo works in this /bin/sh
+case `echo -n` in
+-n)     _echo_n=   _echo_c='\c';;
+*)      _echo_n=-n _echo_c=;;
+esac
+
 
 (autoconf --version) < /dev/null > /dev/null 2>&1 || {
         echo
@@ -15,30 +22,60 @@ PROG="Aleph One"
         DIE=1
 }
 
-(automake --version) < /dev/null > /dev/null 2>&1 || {
+(libtoolize --version) < /dev/null > /dev/null 2>&1 || {
         echo
-        echo "You must have automake installed to compile $PROG."
-        echo "Get ftp://ftp.gnu.org/pub/gnu/automake-1.4.tar.gz"
+        echo "You must have libtool installed to compile $PROG."
+        echo "Get ftp://ftp.gnu.org/pub/gnu/libtool-1.4.tar.gz"
         echo "(or a newer version if it is available)"
         DIE=1
 }
 
-if test "$DIE" -eq 1; then
+(automake --version) < /dev/null > /dev/null 2>&1 || {
+        echo
+        echo "You must have automake installed to compile $PROG."
+        echo "Get ftp://ftp.gnu.org/pub/gnu/automake-1.3.tar.gz"
+        echo "(or a newer version if it is available)"
+        DIE=1
+}
+
+(aclocal --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "**Error**: Missing aclocal. The version of automake"
+	echo "installed doesn't appear recent enough."
+	echo "Get ftp://ftp.gnu.org/pub/gnu/automake-1.3.tar.gz"
+	echo "(or a newer version if it is available)"
+	DIE=1
+}
+
+if [ "$DIE" -eq 1 ]; then
         exit 1
 fi
 
-if test -z "$*"; then
-        echo "I am going to run ./configure with no arguments - if you wish "
-        echo "to pass any to it, please specify them on the $0 command line."
-fi
+aclocalinclude="$ACLOCAL_FLAGS"; \
+(echo $_echo_n " + Running aclocal: $_echo_c"; \
+    aclocal $aclocalinclude; \
+ echo "done.") && \
+(echo $_echo_n " + Running libtoolize: $_echo_c"; \
+    libtoolize --force --copy >/dev/null 2>&1; \
+ echo "done.") && \
+(echo $_echo_n " + Running autoheader: $_echo_c"; \
+    autoheader; \
+ echo "done.") && \
+(echo $_echo_n " + Running automake: $_echo_c"; \
+    automake --gnu --add-missing --copy; \
+ echo "done.") && \
+(echo $_echo_n " + Running autoconf: $_echo_c"; \
+    autoconf; \
+ echo "done.")
 
-for dir in .
-do 
-  echo processing $dir
-  (cd $dir; \
-  aclocalinclude="$ACLOCAL_FLAGS"; \
-  aclocal $aclocalinclude; \
-  autoheader; automake -a; autoconf)
-done
+rm -f config.cache
 
-./configure "$@"
+#if [ x"$NO_CONFIGURE" = "x" ]; then
+#    echo " + Running 'configure $@':"
+#    if [ -z "$*" ]; then
+#	echo "   ** If you wish to pass arguments to ./configure, please"
+#        echo "   ** specify them on the command line."
+#    fi
+#    ./configure "$@" && \
+#    echo "Now type 'make' to compile $PKG_NAME" || exit 1
+#fi
