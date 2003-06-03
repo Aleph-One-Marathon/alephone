0b545dabdadeb779c78fe246e61d4bd38c7f8e67
brefin <brefin@users.sourceforge.net>
2003-06-03 06:10:32 +0000

    Better error message in case the script can't find any of the listed files. (For instance, due to running the script from the wrong directory)
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@1711 a75d40c9-0969-4759-85e5-97cb871b7ed4

--- PBProjects/fix-cvs-resources.sh
+++ PBProjects/fix-cvs-resources.sh
@@ -26,12 +26,14 @@ ROOT_DIR=../
 SINGLE2FORKS="perl ${ROOT_DIR}tools/single2forks.pl"
 APPLESINGLE_FILES='Cheats/Activate%20Cheats Extras/Marathon2_SOCK Extras/MarathonInf_1.2_SOCK Extras/MarathonInf_1.5_SOCK Extras/demo.resource Extras/demos/demos.resource MML%20Scripts/M2%20Map%20Script%20(test) MML%20Scripts/Marathon%202%20Display MML%20Scripts/Marathon%20oo%20Display MML%20Scripts/Text%20Strings Pfhortran_Release/Quartz%20copy Pfhortran_Release/Ten%20Forty%20Two Resources/MV%20Resources Resources/Old%20Stuff Resources/Pfhormz%20rsrc Resources/marathon2.resource Resources/m2_stripped.rsrc tools/MapChunker.rsrc'
 
+found=0
 for i in $APPLESINGLE_FILES;
 do
 	file=`echo $i | sed -e 's/%20/ /g'`;
 	file=$ROOT_DIR$file;
 	if test -e "$file"
 	then
+		found=`expr $found + 1`
 		target_file=$file;
 		if $SINGLE2FORKS "$file";
 		then		
@@ -42,3 +44,8 @@ do
 		fi
 	fi
 done
+if [ $found -eq 0 ]
+then
+	echo fix-cvs-resources.sh was unable to find any of the files it was supposed to fix.\
+Please make sure that you are trying to run it from the aleph/PBProjects directory by cd\'ing to it first.
+fi
