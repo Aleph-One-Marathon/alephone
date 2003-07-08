87f98e19715d3ea42026dd68bb9c3885d1cce3aa
wzenfell <wzenfell@users.sourceforge.net>
2003-07-08 02:02:42 +0000

    [James Willson] Tempering overzealous assert()
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@1842 a75d40c9-0969-4759-85e5-97cb871b7ed4

--- Source_Files/RenderMain/shapes.cpp
+++ Source_Files/RenderMain/shapes.cpp
@@ -816,9 +816,9 @@ void extended_get_shape_bitmap_and_shading_table(
 	short clut_index= GET_COLLECTION_CLUT(collection_code);
 	
 	// Forget about it if some one managed to call us with the NONE value
-	assert(clut_index+1 != MAXIMUM_CLUTS_PER_COLLECTION &&
-		collection_index+1 != MAXIMUM_COLLECTIONS &&
-		low_level_shape_index+1 != MAXIMUM_SHAPES_PER_COLLECTION);
+	assert(!(clut_index+1 == MAXIMUM_CLUTS_PER_COLLECTION &&
+		collection_index+1 == MAXIMUM_COLLECTIONS &&
+		low_level_shape_index+1 == MAXIMUM_SHAPES_PER_COLLECTION));
 	
 	struct low_level_shape_definition *low_level_shape= get_low_level_shape_definition(collection_index, low_level_shape_index);
 	// Return NULL pointers for bitmap and shading table if the frame does not exist
