286fad3e24fee3614ce041f08fa057293170d1b6
wzenfell <wzenfell@users.sourceforge.net>
2005-06-09 05:26:14 +0000

    NIBs metaserver client now displays (albeit poorly) players in room and games in room.  You can't actually do anything with the displays (clicking is ignored).  Code is also a bit rough, but will shift around to be better.  Goal is to have what's currently ListWidget (in NIBsland) and w_list (or some of its subclasses) (in SDLland) share an interface, be smart about using callbacks to delegate some elements of behavior to client code, etc.  Also the string stuff will move to csstrings, or whatever.  And of course other places that can benefit from this stuff should use it instead of what it's using currently.  At some point, maybe once it stabilizes a bit.  (Programming is remarkably painful without automated refactoring tools a la Eclipse, JetBrains' IntelliJ IDEA or ReSharper, etc. :( )
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@2275 a75d40c9-0969-4759-85e5-97cb871b7ed4

--- Source_Files/Network/Metaserver/NibsMetaserverClientUi.cpp
+++ Source_Files/Network/Metaserver/NibsMetaserverClientUi.cpp
@@ -31,6 +31,8 @@
 #include "NibsUiHelpers.h"
 
 enum {
+	iPLAYERS_IN_ROOM = 100,
+	iGAMES_IN_ROOM = 200,
 	iCHAT_HISTORY = 300,
 	iCHAT_ENTRY = 400
 };
@@ -43,12 +45,134 @@ static pascal void MetaserverClientUi_Poller(EventLoopTimerRef, void*)
 	MetaserverClient::pumpAll();
 }
 
+class SelfReleasingCFStringRef
+{
+public:
+	SelfReleasingCFStringRef(CFStringRef newlyCreatedStringRef)
+		: m_stringRef(newlyCreatedStringRef)
+	{}
+
+	CFStringRef StringRef() const {
+		return m_stringRef;
+	}
+
+	~SelfReleasingCFStringRef()
+	{
+		CFRelease(m_stringRef);
+	}
+
+private:
+	CFStringRef m_stringRef;
+	
+	// No reason these couldn't be implemented, but they're not yet.
+	SelfReleasingCFStringRef(const SelfReleasingCFStringRef&);
+	SelfReleasingCFStringRef& operator =(const SelfReleasingCFStringRef&);
+};
+
+auto_ptr<SelfReleasingCFStringRef>
+StringToCFString(const string& s)
+{
+	return auto_ptr<SelfReleasingCFStringRef>(
+		new SelfReleasingCFStringRef(
+			CFStringCreateWithCString(NULL, s.c_str(), NULL)
+		)
+	);
+}
+
+template <typename tElement>
+class ListWidget
+{
+public:
+	ListWidget(ControlRef control)
+		: m_control(control)
+	{
+		SetupCallbacks();
+	}
+
+	void SetItems(const vector<tElement>& items)
+	{
+		m_items = items;
+		ItemsChanged();
+	}
+
+private:
+	vector<tElement>	m_items;
+	ControlRef		m_control;
+
+	void SetupCallbacks()
+	{
+		SetControlReference(m_control, this);
+
+		DataBrowserCallbacks callbacks;
+		obj_clear(callbacks);
+		callbacks.version = kDataBrowserLatestCallbacks;
+		callbacks.u.v1.itemDataCallback = NewDataBrowserItemDataUPP(BounceValueForItemId);
+		//Callbacks.u.v1.itemNotificationCallback = NewDataBrowserItemNotificationUPP(PlayerListMemberHit);
+		SetDataBrowserCallbacks(m_control, &callbacks);
+	}
+	
+	void ClearItemsFromControl()
+	{
+		const DataBrowserItemID container = kDataBrowserNoItem;
+		const UInt32 numItems = 0;
+		const DataBrowserItemID* items = NULL;
+		const DataBrowserPropertyID presortedByProperty = kDataBrowserItemNoProperty;
+		RemoveDataBrowserItems(m_control, container, numItems, items, presortedByProperty);
+	}
+
+	void InstallItemsInControl()
+	{
+		const DataBrowserItemID container = kDataBrowserNoItem;
+		UInt32 numItems = m_items.size();
+		vector<DataBrowserItemID> itemIds(numItems);
+		for (size_t i = 0; i < itemIds.size(); i++)
+			itemIds[i] = i + 1;
+		DataBrowserItemID* items = &(itemIds[0]);
+		const DataBrowserPropertyID presortedByProperty = kDataBrowserItemNoProperty;
+		AddDataBrowserItems(m_control, container, numItems, items, presortedByProperty);
+	}
+
+	void ItemsChanged()
+	{
+		ClearItemsFromControl();
+		InstallItemsInControl();
+	}
+
+	const string ValueForItemId(DataBrowserItemID id)
+	{
+		UInt32 itemIndex = id - 1;
+
+		return (itemIndex < m_items.size()) ? m_items[itemIndex].name() : string();
+	}
+
+	static pascal OSStatus BounceValueForItemId(
+		ControlRef browser,
+		DataBrowserItemID itemId,
+		DataBrowserPropertyID propertyId,
+		DataBrowserItemDataRef itemData,
+		Boolean setValue
+	)
+	{
+		ListWidget<tElement>* listWidget = reinterpret_cast<ListWidget<tElement>*>(GetControlReference(browser));
+		
+		string value = listWidget->ValueForItemId(itemId);
+
+		auto_ptr<SelfReleasingCFStringRef> valueCfString = StringToCFString(value);
+
+		SetDataBrowserItemDataText(itemData, valueCfString->StringRef());
+
+		return noErr;
+	}
+};
+
 class NibsMetaserverClientUi : public MetaserverClientUi, public MetaserverClient::NotificationAdapter
 {
 public:
 	NibsMetaserverClientUi()
 	: m_metaserverClientNib(CFSTR("Metaserver Client"))
 	, m_dialog(m_metaserverClientNib.nibReference(), CFSTR("Metaserver Client"))
+	, m_playersInRoomWidget(GetCtrlFromWindow(m_dialog(), 0, iPLAYERS_IN_ROOM))
+	, m_gamesInRoomWidget(GetCtrlFromWindow(m_dialog(), 0, iGAMES_IN_ROOM))
 	{}
 	
 	~NibsMetaserverClientUi() {}
@@ -71,10 +195,12 @@ public:
 
 	void playersInRoomChanged()
 	{
+		m_playersInRoomWidget.SetItems(m_metaserverClient.playersInRoom());
 	}
 
 	void gamesInRoomChanged()
 	{
+		m_gamesInRoomWidget.SetItems(m_metaserverClient.gamesInRoom());
 	}
 
 	void receivedChatMessage(const std::string& senderName, uint32 senderID, const std::string& message)
@@ -97,9 +223,11 @@ public:
 	}
 
 private:
-	AutoNibReference	m_metaserverClientNib;
-	AutoNibWindow		m_dialog;
-	MetaserverClient	m_metaserverClient;
+	AutoNibReference				m_metaserverClientNib;
+	AutoNibWindow					m_dialog;
+	MetaserverClient				m_metaserverClient;
+	ListWidget<MetaserverPlayerInfo>		m_playersInRoomWidget;
+	ListWidget<GameListMessage::GameListEntry>	m_gamesInRoomWidget;
 };
 
 auto_ptr<MetaserverClientUi>
