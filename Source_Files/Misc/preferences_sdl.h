2964421dcdd95bdef444504cbb8957e27ce65aef
ghs <ghs@users.sourceforge.net>
2006-03-10 01:44:05 +0000

    NetBSD can use getlogin(), but doesn't define unix or __unix__
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@2612 a75d40c9-0969-4759-85e5-97cb871b7ed4

--- Source_Files/Misc/preferences_sdl.h
+++ Source_Files/Misc/preferences_sdl.h
@@ -106,7 +106,7 @@ static void get_name_from_system(unsigned char *outName)
     // Hope caller's buffer is big enough.
     char* name = (char*) outName;
 
-#if defined(unix) || defined(__BEOS__) || (defined (__APPLE__) && defined (__MACH__))
+#if defined(unix) || defined(__BEOS__) || (defined (__APPLE__) && defined (__MACH__)) || defined(__NetBSD__)
 
 	char *login = getlogin();
 	strcpy(name, login ? login : "Bob User");
