1625aa7b54bdb00a84f4055e4aa0a963adb2c90c
ghs <ghs@users.sourceforge.net>
2006-04-02 14:08:19 +0000

    fix checkpoint tag rendering with SDL/OpenGL terminals
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@2638 a75d40c9-0969-4759-85e5-97cb871b7ed4

--- Source_Files/RenderOther/OverheadMap_SDL.cpp
+++ Source_Files/RenderOther/OverheadMap_SDL.cpp
@@ -30,7 +30,7 @@
 
 
 // From screen_sdl.cpp
-extern SDL_Surface *world_pixels;
+extern SDL_Surface *draw_surface;
 
 
 /*
@@ -53,10 +53,10 @@ void OverheadMap_SDL_Class::draw_polygon(short vertex_count, short *vertices, rg
 		vertex_array[i] = GetVertex(vertices[i]);
 
 	// Get color
-	uint32 pixel = SDL_MapRGB(world_pixels->format, color.red >> 8, color.green >> 8, color.blue >> 8);
+	uint32 pixel = SDL_MapRGB(draw_surface->format, color.red >> 8, color.green >> 8, color.blue >> 8);
 
 	// Draw polygon
-	::draw_polygon(world_pixels, vertex_array, vertex_count, pixel);
+	::draw_polygon(draw_surface, vertex_array, vertex_count, pixel);
 }
 
 
@@ -71,10 +71,10 @@ void OverheadMap_SDL_Class::draw_line(short *vertices, rgb_color &color, short p
 	world_point2d *v2 = &GetVertex(vertices[1]);
 
 	// Get color
-	uint32 pixel = SDL_MapRGB(world_pixels->format, color.red >> 8, color.green >> 8, color.blue >> 8);
+	uint32 pixel = SDL_MapRGB(draw_surface->format, color.red >> 8, color.green >> 8, color.blue >> 8);
 
 	// Draw line
-	::draw_line(world_pixels, v1, v2, pixel, pen_size);
+	::draw_line(draw_surface, v1, v2, pixel, pen_size);
 }
 
 
@@ -90,12 +90,12 @@ void OverheadMap_SDL_Class::draw_thing(world_point2d &center, rgb_color &color, 
 	SDL_Rect r = {center.x - raddown, center.y - raddown, radup, radup};
 
 	// Get color
-	uint32 pixel = SDL_MapRGB(world_pixels->format, color.red >> 8, color.green >> 8, color.blue >> 8);
+	uint32 pixel = SDL_MapRGB(draw_surface->format, color.red >> 8, color.green >> 8, color.blue >> 8);
 
 	// Draw thing
 	switch (shape) {
 		case _rectangle_thing:
-			SDL_FillRect(world_pixels, &r, pixel);
+			SDL_FillRect(draw_surface, &r, pixel);
 			break;
 		case _circle_thing: {
 			world_point2d circle[8] = {
@@ -109,8 +109,8 @@ void OverheadMap_SDL_Class::draw_thing(world_point2d &center, rgb_color &color, 
 				{int16(-0.3 * radius) + center.x, int16(-0.75 * radius) + center.y}
 			};
 			for (int i=0; i<7; i++)
-				::draw_line(world_pixels, circle + i, circle + i + 1, pixel, 2);
-			::draw_line(world_pixels, circle + 7, circle, pixel, 2);
+				::draw_line(draw_surface, circle + i, circle + i + 1, pixel, 2);
+			::draw_line(draw_surface, circle + 7, circle, pixel, 2);
 			break;
 		}
 	}
@@ -131,10 +131,10 @@ void OverheadMap_SDL_Class::draw_player(world_point2d &center, angle facing, rgb
 	translate_point2d(triangle + 2, rear >> shrink, normalize_angle(facing - rear_theta));
 
 	// Get color
-	uint32 pixel = SDL_MapRGB(world_pixels->format, color.red >> 8, color.green >> 8, color.blue >> 8);
+	uint32 pixel = SDL_MapRGB(draw_surface->format, color.red >> 8, color.green >> 8, color.blue >> 8);
 
 	// Draw triangle
-	::draw_polygon(world_pixels, triangle, 3, pixel);
+	::draw_polygon(draw_surface, triangle, 3, pixel);
 }
 
 
@@ -154,10 +154,10 @@ void OverheadMap_SDL_Class::draw_text(world_point2d &location, rgb_color &color,
 		xpos -= text_width(text, font, style) / 2;
 	
 	// Get color
-	uint32 pixel = SDL_MapRGB(world_pixels->format, color.red >> 8, color.green >> 8, color.blue >> 8);
+	uint32 pixel = SDL_MapRGB(draw_surface->format, color.red >> 8, color.green >> 8, color.blue >> 8);
 
 	// Draw text
-	::draw_text(world_pixels, text, xpos, location.y, pixel, font, style);
+	::draw_text(draw_surface, text, xpos, location.y, pixel, font, style);
 }
 
 
@@ -167,12 +167,12 @@ void OverheadMap_SDL_Class::draw_text(world_point2d &location, rgb_color &color,
 
 void OverheadMap_SDL_Class::set_path_drawing(rgb_color &color)
 {
-	path_pixel = SDL_MapRGB(world_pixels->format, color.red >> 8, color.green >> 8, color.blue >> 8);
+	path_pixel = SDL_MapRGB(draw_surface->format, color.red >> 8, color.green >> 8, color.blue >> 8);
 }
 
 void OverheadMap_SDL_Class::draw_path(short step, world_point2d &location)
 {
 	if (step)
-		::draw_line(world_pixels, &path_point, &location, path_pixel, 1);
+		::draw_line(draw_surface, &path_point, &location, path_pixel, 1);
 	path_point = location;
 }
