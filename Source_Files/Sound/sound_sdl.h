8dba4531dad4c9b89896a0811be6e599f7e54b53
ghs <ghs@users.sourceforge.net>
2006-08-12 13:30:12 +0000

    fix crash playing startup music if sound can not be initialized or is disable
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@2748 a75d40c9-0969-4759-85e5-97cb871b7ed4

--- Source_Files/Sound/sound_sdl.h
+++ Source_Files/Sound/sound_sdl.h
@@ -346,6 +346,7 @@ bool load_music (FileSpecifier &song_file)
 #ifdef MUSIC_SDL
 bool load_music_sdl(FileSpecifier &song_file)
 {
+	if (!_sm_initialized || !_sm_active) return false;
 	sdl_channel *c = sdl_channels + MUSIC_CHANNEL;	// Music channel
 	uint32 music_sample_rate;						// Music sample rate in Hz
 
