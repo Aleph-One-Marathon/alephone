2ea0ca079407ed5e168a8b127545495033d49692
ghs <ghs@users.sourceforge.net>
2006-09-03 14:17:27 +0000

    don't try to guess what the server made up for us if we get an early ack
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@2779 a75d40c9-0969-4759-85e5-97cb871b7ed4

--- Source_Files/Network/network_star_spoke.cpp
+++ Source_Files/Network/network_star_spoke.cpp
@@ -451,21 +451,7 @@ spoke_received_game_data_packet_v1(AIStream& ps, bool reflected_flags)
 	// we can get an early ACK if the server made up flags for us...
 	if (theSmallestUnacknowledgedTick > sOutgoingFlags.getWriteTick())
 	{
-		logTraceNMT2("early ack (%d > %d); filling outgoing queue", theSmallestUnacknowledgedTick, sOutgoingFlags.getWriteTick());
-
-		// guess what it made up for us
-		// we'll probably be wrong
-		action_flags_t flags = sOutgoingFlags.peek(sOutgoingFlags.getReadTick());
-		
-		while (sOutgoingFlags.getWriteTick() < theSmallestUnacknowledgedTick)
-		{
-			if (sOutgoingFlags.getWriteTick() >= sSmallestRealGameTick)
-				sLocallyGeneratedFlags.enqueue(flags);
-			else
-				
-				sOutgoingFlags.enqueue(flags);
-		}
-		
+		theSmallestUnacknowledgedTick = sOutgoingFlags.getWriteTick();
 	}
 
         // Heard from hub
