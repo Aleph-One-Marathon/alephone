d83abffefacec514b607f5f14c3030cfe0550c46
ghs <ghs@users.sourceforge.net>
2006-11-07 02:23:36 +0000

    "fix" seams in tiled images
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@2864 a75d40c9-0969-4759-85e5-97cb871b7ed4

--- Source_Files/RenderOther/OGL_Blitter.cpp
+++ Source_Files/RenderOther/OGL_Blitter.cpp
@@ -63,8 +63,8 @@ OGL_Blitter::OGL_Blitter(const SDL_Surface& s, const SDL_Rect& dst, const SDL_Re
 
 			glBindTexture(GL_TEXTURE_2D, m_refs[i]);
 
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 			
