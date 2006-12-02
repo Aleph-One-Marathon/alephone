e0974dad7a9603b28f0c42be5623d6de0cf53e18
ghs <ghs@users.sourceforge.net>
2006-12-02 21:42:06 +0000

    with Woody's approval, update copyright notices of some of his files to add GPL
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@2892 a75d40c9-0969-4759-85e5-97cb871b7ed4

--- Source_Files/Misc/thread_priority_sdl.h
+++ Source_Files/Misc/thread_priority_sdl.h
@@ -1,10 +1,22 @@
 /*
  *  thread_priority_sdl.h
- *  AlephOne-OSX
- *
- *  Created by woody on Sat Dec 01 2001.
- *  Copyright (c) 2001 __MyCompanyName__. All rights reserved.
- *
+
+	Copyright (C) 2001 and beyond by Woody Zenfell, III
+	and the "Aleph One" developers.
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	This license is contained in the file "COPYING",
+	which is included with this source code; it is available online at
+	http://www.gnu.org/licenses/gpl.html
  */
 
 #ifndef THREAD_PRIORITY_H
