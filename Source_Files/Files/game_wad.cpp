0b961dae5df6d3ea45a14134b449274f072da756
ghs <ghs@users.sourceforge.net>
2006-12-03 13:49:26 +0000

    fix initial loading of custom physics
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@2902 a75d40c9-0969-4759-85e5-97cb871b7ed4

--- Source_Files/Files/game_wad.cpp
+++ Source_Files/Files/game_wad.cpp
@@ -137,7 +137,7 @@ FileSpecifier MapFileSpec;
 static bool file_is_set= false;
 
 // LP addition: was a physics model loaded from the previous level loaded?
-static bool PhysicsModelLoadedEarlier = false;
+static bool PhysicsModelLoadedEarlier = true;
 
 // The following local globals are for handling games that need to be restored.
 struct revert_game_info
