f32789a3c2583e4c49b889670f65f3878c2ce494
ghs <ghs@users.sourceforge.net>
2007-01-18 02:04:06 +0000

    moved computer_interface_sdl.h stuff into computer_interface.cpp deleted computer_interface_mac.h and computer_interface_sdl.h
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@2938 a75d40c9-0969-4759-85e5-97cb871b7ed4

--- Source_Files/RenderOther/computer_interface.cpp
+++ Source_Files/RenderOther/computer_interface.cpp
@@ -367,14 +367,115 @@ static void encode_text(terminal_text_t *terminal_text);
 static void decode_text(terminal_text_t *terminal_text);
 #endif
 
-/* ------------ machine-specific code */
+#include "sdl_fonts.h"
 
-#if defined(mac)
-#include "computer_interface_mac.h"
-#elif defined(SDL)
-#include "computer_interface_sdl.h"
+
+// Global variables
+// static const sdl_font_info *terminal_font = NULL;
+static uint32 current_pixel;				// Current color pixel value
+static uint16 current_style = styleNormal;	// Current style flags
+
+// From screen_sdl.cpp
+extern SDL_Surface *world_pixels;
+
+
+// Terminal key definitions
+static struct terminal_key terminal_keys[]= {
+	{SDLK_UP, 0, 0, _terminal_page_up},				// arrow up
+	{SDLK_DOWN, 0, 0, _terminal_page_down},			// arrow down
+	{SDLK_PAGEUP, 0, 0, _terminal_page_up},			// page up
+	{SDLK_PAGEDOWN, 0, 0, _terminal_page_down},		// page down
+	{SDLK_TAB, 0, 0, _terminal_next_state},			// tab
+	{SDLK_KP_ENTER, 0, 0, _terminal_next_state},	// enter
+	{SDLK_RETURN, 0, 0, _terminal_next_state},		// return
+	{SDLK_SPACE, 0, 0, _terminal_next_state},		// space
+	{SDLK_ESCAPE, 0, 0, _any_abort_key_mask}		// escape
+};
+
+
+// Emulation of MacOS functions
+static void SetRect(Rect *r, int left, int top, int right, int bottom)
+{
+	r->top = top;
+	r->left = left;
+	r->bottom = bottom;
+	r->right = right;
+}
+
+static void InsetRect(Rect *r, int dx, int dy)
+{
+	r->top += dy;
+	r->left += dx;
+	r->bottom -= dy;
+	r->right -= dx;
+}
+
+static void OffsetRect(Rect *r, int dx, int dy)
+{
+	r->top += dy;
+	r->left += dx;
+	r->bottom += dy;
+	r->right += dx;
+}
+
 extern SDL_Surface *draw_surface;
-#endif
+
+static void	set_text_face(struct text_face_data *text_face)
+{
+	current_style = styleNormal;
+
+	// Set style
+	if (text_face->face & _bold_text)
+		current_style |= styleBold;
+	if (text_face->face & _italic_text)
+		current_style |= styleItalic;
+	if (text_face->face & _underline_text)
+		current_style |= styleUnderline;
+
+	// Set color
+	SDL_Color color;
+	_get_interface_color(text_face->color + _computer_interface_text_color, &color);
+	current_pixel = SDL_MapRGB(/*world_pixels*/draw_surface->format, color.r, color.g, color.b);
+}
+
+
+static bool calculate_line(char *base_text, short width, short start_index, short text_end_index, short *end_index)
+{
+	bool done = false;
+
+	if (base_text[start_index]) {
+		int index = start_index, running_width = 0;
+		
+		// terminal_font no longer a global, since it may change
+		sdl_font_info *terminal_font = GetInterfaceFont(_computer_interface_font);
+
+		while (running_width < width && base_text[index] && base_text[index] != MAC_LINE_END) {
+			running_width += char_width(base_text[index], terminal_font, current_style);
+			index++;
+		}
+		
+		// Now go backwards, looking for whitespace to split on
+		if (base_text[index] == MAC_LINE_END)
+			index++;
+		else if (base_text[index]) {
+			int break_point = index;
+
+			while (break_point>start_index) {
+				if (base_text[break_point] == ' ')
+					break; 	// Non printing
+				break_point--;	// this needs to be in front of the test
+			}
+			
+			if (break_point != start_index)
+				index = break_point+1;	// Space at the end of the line
+		}
+		
+		*end_index= index;
+	} else
+		done = true;
+	
+	return done;
+}
 
 /* ------------ code begins */
 
