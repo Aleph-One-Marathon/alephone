705ef90c51fd448b532d3421d713b9299cd280be
ghs <ghs@users.sourceforge.net>
2007-01-18 02:12:46 +0000

    move game_window_sdl.cpp stuff into game_window.cpp delete game_window_sdl.cpp and game_window_macintosh.cpp
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@2939 a75d40c9-0969-4759-85e5-97cb871b7ed4

--- Source_Files/RenderOther/game_window.cpp
+++ Source_Files/RenderOther/game_window.cpp
@@ -82,6 +82,12 @@ Mar 08, 2002 (Woody Zenfell):
 #include "FontHandler.h"
 #include "screen.h"
 
+#include "shell.h"
+#include "preferences.h"
+#include "screen.h"
+#include "screen_definitions.h"
+#include "images.h"
+
 #include    "network_sound.h"
 
 #ifdef HAVE_OPENGL
@@ -487,6 +493,65 @@ static void set_current_inventory_screen(
 	player->interface_decay= 5*TICKS_PER_SECOND;
 }
 
+// From sreen_sdl.cpp
+extern SDL_Surface *HUD_Buffer;
+extern void build_sdl_color_table(const color_table *color_table, SDL_Color *colors);
+
+// From game_window.cpp
+extern HUD_SW_Class HUD_SW;
+extern bool OGL_HUDActive;
+
+extern void draw_panels(void);
+
+void ensure_HUD_buffer(void) {
+
+  // Allocate surface for HUD if not present
+  if (HUD_Buffer == NULL) {
+    SDL_Surface *s = SDL_CreateRGBSurface(SDL_SWSURFACE, 640, 480, 8, 0xff, 0xff, 0xff, 0xff);
+    if (s == NULL)
+      alert_user(fatalError, strERRORS, outOfMemory, -1);
+    HUD_Buffer = SDL_DisplayFormat(s);
+    if (HUD_Buffer == NULL)
+      alert_user(fatalError, strERRORS, outOfMemory, -1);
+    SDL_FreeSurface(s);
+  }
+}
+
+/*
+ *  Draw HUD (to HUD surface)
+ */
+
+void draw_panels(void)
+{
+	if (OGL_HUDActive)
+		return;
+
+	ensure_HUD_buffer();
+
+	// Draw static HUD picture
+	static SDL_Surface *static_hud_pict = NULL;
+	static bool hud_pict_not_found = false;
+	if (static_hud_pict == NULL && !hud_pict_not_found) {
+		LoadedResource rsrc;
+		if (get_picture_resource_from_images(INTERFACE_PANEL_BASE, rsrc))
+			static_hud_pict = picture_to_surface(rsrc);
+		else
+			hud_pict_not_found = true;
+	} 
+
+	if (!hud_pict_not_found) {
+		SDL_Rect dst_rect = {0, 320, 640, 160};
+		SDL_BlitSurface(static_hud_pict, NULL, HUD_Buffer, &dst_rect);
+	}
+
+	// Add dynamic elements
+	_set_port_to_HUD();
+	HUD_SW.update_everything(NONE);
+	_restore_port();
+
+	// Tell main loop to render the HUD in the next run
+	RequestDrawingHUD();
+}
 
 /* MML parser for the 'vidmaster' tag */
 extern short vidmasterStringSetID; // shell.cpp
