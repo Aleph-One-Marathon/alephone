80c14bc152f076bbd723c744924aaa73632da347
ghs <ghs@users.sourceforge.net>
2007-04-28 01:19:03 +0000

    only re-interpret loop as frames if the loop length is >= 4
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@3022 a75d40c9-0969-4759-85e5-97cb871b7ed4

--- Source_Files/Sound/SoundFile.cpp
+++ Source_Files/Sound/SoundFile.cpp
@@ -103,7 +103,7 @@ bool SoundHeader::UnpackExtendedSystem7Header(AIStreamBE &header)
 		length = num_frames * bytes_per_frame;
 		little_endian = false;
 
-		if ((loop_start % bytes_per_frame) || (loop_end % bytes_per_frame))
+		if ((loop_end - loop_start >= 4) && ((loop_start % bytes_per_frame) || (loop_end % bytes_per_frame)))
 		{
 			logWarning3("loop_start=%i and loop_end=%i but bytes_per_frame=%i; interpreting as frame offsets", loop_start, loop_end, bytes_per_frame);
 			loop_start *= bytes_per_frame;
