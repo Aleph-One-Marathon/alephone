ba8562b5e96bf46e461ef47a78eee44a03a947cf
ghs <ghs@users.sourceforge.net>
2007-05-12 14:03:03 +0000

    make alephversion useful again
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@3042 a75d40c9-0969-4759-85e5-97cb871b7ed4

--- Source_Files/Misc/alephversion.h
+++ Source_Files/Misc/alephversion.h
@@ -1,92 +1,45 @@
 #ifndef __ALEPHVERSION_H
 #define __ALEPHVERSION_H
 
-// ZZZ: screw this stuff - nobody's keeping it up to date.
-/*
-	#define MAJOR_VERSION   1
-	#define MAJOR_VERSION_S "1"
-	#define MINOR_VERSION   0
-	#define MINOR_VERSION_S "0"
-	#define STEPPING        "a"
-	#define BUILDNUM        1
-	#define BUILDSTR        "1"
-	#define BRANCH          "(trunk)"
-	#define FINAL	        0
-	
-	#if FINAL
-		#define VERSION_STRING MAJOR_VERSION_S "." MINOR_VERSION_S STEPPING BUILDSTR BRANCH
-	#else
-		#define VERSION_STRING MAJOR_VERSION_S "." MINOR_VERSION_S STEPPING BUILDSTR "pre" BRANCH "(" __DATE__ " " __TIME__ ")"
-	#endif
+/* If you change the version number here, you should also change it in:
+
+   configure.ac
+   Resources/Windows/alephone.rc
+   Resources/Aleph One Classic SDL.r
+   PBProjects/English.lproj/InfoPlist.strings
+
 */
 
+#define A1_DISPLAY_VERSION "0.18pre2"
+#define A1_DISPLAY_DATE_VERSION "2007-04-29"
+#define A1_DATE_VERSION "20070429"
 
-#ifndef A1_VERSION_PLATFORM
-
-
-# ifdef SDL
-
-#  ifndef A1_VERSION_MINOR_PLATFORM
-
-#   ifdef WIN32
-#    define A1_VERSION_MINOR_PLATFORM "Windows"
-#   elif defined(__APPLE__)
-#    define A1_VERSION_MINOR_PLATFORM "Mac OS X"
-#   elif defined(__MACOS__)
-#    define A1_VERSION_MINOR_PLATFORM "Mac OS"
-#   elif defined(TARGET_PLATFORM)
-#    define A1_VERSION_MINOR_PLATFORM TARGET_PLATFORM
-// add more minor platforms (and/or a general way of getting GNU-style arch/OS stuff) here
-#   endif
-
-#   ifndef A1_VERSION_MINOR_PLATFORM
-#    define A1_VERSION_MINOR_PLATFORM "unknown platform"
-#   endif
-
-#  endif // !defined(A1_VERSION_MINOR_PLATFORM)
-
-#  define A1_VERSION_PLATFORM "SDL " A1_VERSION_MINOR_PLATFORM
-
-
-# else // now !defined(SDL)
-
-
-#  ifndef A1_VERSION_MINOR_PLATFORM
-
-#   ifdef TARGET_API_MAC_CARBON
-#    ifdef __APPLE_CC__
-#     define A1_VERSION_MINOR_PLATFORM "Carbon (PB)"
-#    else
-#     ifdef __MWERKS__
-#      define A1_VERSION_MINOR_PLATFORM "Carbon (CW)"
-#     else
-#      define A1_VERSION_MINOR_PLATFORM "Carbon (Other)"
-#     endif
-#    endif
-#   else
-#    define A1_VERSION_MINOR_PLATFORM "Classic"
-#   endif
-
-#  endif // !defined(A1_VERSION_MINOR_PLATFORM)
-
-#  define A1_VERSION_PLATFORM "Mac OS " A1_VERSION_MINOR_PLATFORM
-
-
-# endif // !defined(SDL)
-
-
-#endif // !defined(A1_VERSION_PLATFORM)
-
-
-
-#ifndef A1_VERSION_PLATFORM
-# define A1_VERSION_PLATFORM "unknown platform"
+#ifdef WIN32
+#define A1_DISPLAY_PLATFORM "Windows"
+#define A1_UPDATE_PLATFORM "windows"
+#elif defined (__APPLE__) && defined(__MACH__)
+#define A1_DISPLAY_PLATFORM "Mac OS X"
+#define A1_UPDATE_PLATFORM "macosx"
+#elif defined (__MACOS__)
+#define A1_DISPLAY_PLATFORM "Mac OS"
+#define A1_UPDATE_PLATFORM "macos"
+#elif defined (linux)
+#define A1_DISPLAY_PLATFORM "Linux"
+#define A1_UPDATE_PLATFORM "source"
+#elif defined (__BEOS__)
+#define A1_DISPLAY_PLATFORM "BeOS"
+#define A1_UPDATE_PLATFORM "source"
+#elif defined (__NETBSD__)
+#define A1_DISPLAY_PLATFORM "NetBSD"
+#define A1_UPDATE_PLATFORM "source"
+#else
+#define A1_DISPLAY_PLATFORM "Unknown"
+#define A1_UPDATE_PLATFORM "source"
 #endif
 
 #ifndef A1_VERSION_STRING
-# define A1_VERSION_STRING A1_VERSION_PLATFORM " " __DATE__
-#endif // !defined(A1_VERSION_STRING)
-
+#define A1_VERSION_STRING "Aleph One " A1_DISPLAY_PLATFORM " " A1_DISPLAY_DATE_VERSION " " A1_DISPLAY_VERSION
+#endif
 
 
 #endif // ALEPHVERSION_H
