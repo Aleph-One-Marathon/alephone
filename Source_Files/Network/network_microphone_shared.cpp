4688bb61aa8504c7764e4a48199ea1b68c4ae214
ghs <ghs@users.sourceforge.net>
2007-10-20 17:41:11 +0000

    return true out of announce_microphone_capture_format, because we accept everything now
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@3111 a75d40c9-0969-4759-85e5-97cb871b7ed4

--- Source_Files/Network/network_microphone_shared.cpp
+++ Source_Files/Network/network_microphone_shared.cpp
@@ -81,6 +81,8 @@ bool announce_microphone_capture_format(uint32 inSamplesPerSecond, bool inStereo
 
 	sNumberOfBytesPerSample = (inStereo ? 2 : 1) * (in16Bit ? 2 : 1);
 	sCaptureBytesPerPacket = (uint32) ((rate * kNetworkAudioSamplesPerPacket) >> 16) * sNumberOfBytesPerSample;
+
+	return true;
 };
 
 
