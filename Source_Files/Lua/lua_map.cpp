fa206e86fcda8eaed8b4026b4ccbc43e8768ea8f
ghs <ghs@users.sourceforge.net>
2008-01-24 01:22:32 +0000

    subtables for polygon floor/height
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@3209 a75d40c9-0969-4759-85e5-97cb871b7ed4

--- Source_Files/Lua/lua_map.cpp
+++ Source_Files/Lua/lua_map.cpp
@@ -26,7 +26,7 @@ LUA_MAP.CPP
 
 #ifdef HAVE_LUA
 
-struct Lua_Polygon {
+struct Lua_Polygon_Floor {
 	short index;
 	static const char *name;
 
@@ -34,57 +34,42 @@ struct Lua_Polygon {
 	static const luaL_reg index_table[];
 	static const luaL_reg newindex_table[];
 
-	static int get_ceiling_height(lua_State *L);
-	static int get_floor_height(lua_State *L);
-	static int get_type(lua_State *L);
-	static int set_ceiling_height(lua_State *L);
-	static int set_floor_height(lua_State *L);
-	static int set_type(lua_State *L);
+	static int get_height(lua_State *L);
+	static int set_height(lua_State *L);
 };
 
-int Lua_Polygon::get_ceiling_height(lua_State *L)
+const char* Lua_Polygon_Floor::name = "polygon_floor";
+
+const luaL_reg Lua_Polygon_Floor::index_table[] = {
+	{"height", Lua_Polygon_Floor::get_height},
+	{0, 0}
+};
+
+const luaL_reg Lua_Polygon_Floor::newindex_table[] = {
+	{"height", Lua_Polygon_Floor::set_height},
+	{0, 0}
+};
+
+const luaL_reg Lua_Polygon_Floor::metatable[] = {
+	{"__index", L_TableGet<Lua_Polygon_Floor>},
+	{"__newindex", L_TableSet<Lua_Polygon_Floor>},
+	{0, 0}
+};
+
+int Lua_Polygon_Floor::get_height(lua_State *L)
 {
-	lua_pushnumber(L, (double) (get_polygon_data(L_Index<Lua_Polygon>(L, 1))->ceiling_height) / WORLD_ONE);
+	lua_pushnumber(L, (double) (get_polygon_data(L_Index<Lua_Polygon_Floor>(L, 1))->floor_height) / WORLD_ONE);
 	return 1;
 }
 
-int Lua_Polygon::get_floor_height(lua_State *L)
-{
-	lua_pushnumber(L, (double) (get_polygon_data(L_Index<Lua_Polygon>(L, 1))->floor_height) / WORLD_ONE);
-	return 1;
-}
-
-int Lua_Polygon::get_type(lua_State *L)
-{
-	lua_pushnumber(L, get_polygon_data(L_Index<Lua_Polygon>(L, 1))->type);
-	return 1;
-}
-
-int Lua_Polygon::set_ceiling_height(lua_State *L)
+int Lua_Polygon_Floor::set_height(lua_State *L)
 {
 	if (!lua_isnumber(L, 2))
 	{
-		luaL_error(L, "polygon.ceiling_height: incorrect argument type");
+		luaL_error(L, "height: incorrect argument type");
 	}
 
-	struct polygon_data *polygon = get_polygon_data(L_Index<Lua_Polygon>(L, 1));
-	polygon->ceiling_height = static_cast<world_distance>(lua_tonumber(L,2)*WORLD_ONE);
-	for (short i = 0; i < polygon->vertex_count; ++i)
-	{
-		recalculate_redundant_endpoint_data(polygon->endpoint_indexes[i]);
-		recalculate_redundant_line_data(polygon->line_indexes[i]);
-	}
-	return 0;
-}
-
-int Lua_Polygon::set_floor_height(lua_State *L)
-{
-	if (!lua_isnumber(L, 2))
-	{
-		luaL_error(L, "polygon.floor_height: incorrect argument type");
-	}
-
-	struct polygon_data *polygon = get_polygon_data(L_Index<Lua_Polygon>(L, 1));
+	struct polygon_data *polygon = get_polygon_data(L_Index<Lua_Polygon_Floor>(L, 1));
 	polygon->floor_height = static_cast<world_distance>(lua_tonumber(L,2)*WORLD_ONE);
 	for (short i = 0; i < polygon->vertex_count; ++i)
 	{
@@ -94,11 +79,96 @@ int Lua_Polygon::set_floor_height(lua_State *L)
 	return 0;
 }
 
+struct Lua_Polygon_Ceiling {
+	short index;
+	static const char *name;
+
+	static const luaL_reg metatable[];
+	static const luaL_reg index_table[];
+	static const luaL_reg newindex_table[];
+
+	static int get_height(lua_State *L);
+	static int set_height(lua_State *L);
+};
+
+const char* Lua_Polygon_Ceiling::name = "polygon_ceiling";
+
+const luaL_reg Lua_Polygon_Ceiling::index_table[] = {
+	{"height", Lua_Polygon_Ceiling::get_height},
+	{0, 0}
+};
+
+const luaL_reg Lua_Polygon_Ceiling::newindex_table[] = {
+	{"height", Lua_Polygon_Ceiling::set_height},
+	{0, 0}
+};
+
+const luaL_reg Lua_Polygon_Ceiling::metatable[] = {
+	{"__index", L_TableGet<Lua_Polygon_Ceiling>},
+	{"__newindex", L_TableSet<Lua_Polygon_Ceiling>},
+	{0, 0}
+};
+
+int Lua_Polygon_Ceiling::get_height(lua_State *L)
+{
+	lua_pushnumber(L, (double) (get_polygon_data(L_Index<Lua_Polygon_Ceiling>(L, 1))->ceiling_height) / WORLD_ONE);
+	return 1;
+}
+
+int Lua_Polygon_Ceiling::set_height(lua_State *L)
+{
+	if (!lua_isnumber(L, 2))
+	{
+		luaL_error(L, "height: incorrect argument type");
+	}
+
+	struct polygon_data *polygon = get_polygon_data(L_Index<Lua_Polygon_Ceiling>(L, 1));
+	polygon->ceiling_height = static_cast<world_distance>(lua_tonumber(L,2)*WORLD_ONE);
+	for (short i = 0; i < polygon->vertex_count; ++i)
+	{
+		recalculate_redundant_endpoint_data(polygon->endpoint_indexes[i]);
+		recalculate_redundant_line_data(polygon->line_indexes[i]);
+	}
+	return 0;
+}
+
+struct Lua_Polygon {
+	short index;
+	static const char *name;
+
+	static const luaL_reg metatable[];
+	static const luaL_reg index_table[];
+	static const luaL_reg newindex_table[];
+
+	static int get_ceiling(lua_State *L);
+	static int get_floor(lua_State *L);
+	static int get_type(lua_State *L);
+	static int set_type(lua_State *L);
+};
+
+int Lua_Polygon::get_ceiling(lua_State *L)
+{
+	L_Push<Lua_Polygon_Ceiling>(L, L_Index<Lua_Polygon>(L, 1));
+	return 1;
+}
+
+int Lua_Polygon::get_floor(lua_State *L)
+{
+	L_Push<Lua_Polygon_Floor>(L, L_Index<Lua_Polygon>(L, 1));
+	return 1;
+}
+
+int Lua_Polygon::get_type(lua_State *L)
+{
+	lua_pushnumber(L, get_polygon_data(L_Index<Lua_Polygon>(L, 1))->type);
+	return 1;
+}
+
 int Lua_Polygon::set_type(lua_State *L)
 {
 	if (!lua_isnumber(L, 2))
 	{
-		luaL_error(L, "polygon.type: incorrect argument type");
+		luaL_error(L, "type: incorrect argument type");
 	}
 
 	int type = static_cast<int>(lua_tonumber(L, 2));
@@ -110,16 +180,14 @@ int Lua_Polygon::set_type(lua_State *L)
 const char *Lua_Polygon::name = "polygon";
 
 const luaL_reg Lua_Polygon::index_table[] = {
-	{"ceiling_height", Lua_Polygon::get_ceiling_height},
-	{"floor_height", Lua_Polygon::get_floor_height},
+	{"ceiling", Lua_Polygon::get_ceiling},
+	{"floor", Lua_Polygon::get_floor},
 	{"index", L_TableIndex<Lua_Polygon>},
 	{"type", Lua_Polygon::get_type},
 	{0, 0}
 };
 
 const luaL_reg Lua_Polygon::newindex_table[] = {
-	{"ceiling_height", Lua_Polygon::set_ceiling_height},
-	{"floor_height", Lua_Polygon::set_floor_height},
 	{"type", Lua_Polygon::set_type},
 	{0, 0}
 };
@@ -150,6 +218,8 @@ const luaL_reg Lua_Polygons::metatable[] = {
 
 int Lua_Map_register(lua_State *L)
 {
+	L_Register<Lua_Polygon_Floor>(L);
+	L_Register<Lua_Polygon_Ceiling>(L);
 	L_Register<Lua_Polygon>(L);
 	L_GlobalRegister<Lua_Polygons>(L);
 
@@ -157,11 +227,11 @@ int Lua_Map_register(lua_State *L)
 }
 
 static const char* compatibility_script = ""
-	"function get_polygon_ceiling_height(polygon) return Polygons[polygon].ceiling_height end\n"
-	"function get_polygon_floor_height(polygon) return Polygons[polygon].floor_height end\n"
+	"function get_polygon_ceiling_height(polygon) return Polygons[polygon].ceiling.height end\n"
+	"function get_polygon_floor_height(polygon) return Polygons[polygon].floor.height end\n"
 	"function get_polygon_type(polygon) return Polygons[polygon].type end\n"
-	"function set_polygon_ceiling_height(polygon, height) Polygons[polygon].ceiling_height = height end\n"
-	"function set_polygon_floor_height(polygon, height) Polygons[polygon].floor_height = height end\n"
+	"function set_polygon_ceiling_height(polygon, height) Polygons[polygon].ceiling.height = height end\n"
+	"function set_polygon_floor_height(polygon, height) Polygons[polygon].floor.height = height end\n"
 	"function set_polygon_type(polygon, type) Polygons[polygon].type = type end\n"
 	;
 
