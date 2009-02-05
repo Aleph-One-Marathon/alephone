6f697e43842f45c96389e64387d29efe65de0679
Gregory Smith <wolfy@treellama.org>
2009-02-05 01:50:54 +0000

    row-order shapes are stored sideways, so switch height/width
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@3708 a75d40c9-0969-4759-85e5-97cb871b7ed4

--- Source_Files/RenderMain/shapes.cpp
+++ Source_Files/RenderMain/shapes.cpp
@@ -460,7 +460,7 @@ SDL_Surface *get_shape_surface(int shape, int inCollection, byte** outPointerToP
 	} else {
 
 		// Row-order shape, we can directly create a surface from it
-		s = SDL_CreateRGBSurfaceFrom(bitmap->row_addresses[0], bitmap->width, bitmap->height, 8, bitmap->bytes_per_row, 0xff, 0xff, 0xff, 0xff);
+		s = SDL_CreateRGBSurfaceFrom(bitmap->row_addresses[0], bitmap->height, bitmap->width, 8, bitmap->bytes_per_row, 0xff, 0xff, 0xff, 0xff);
                 // ZZZ: caller should not dispose of any additional data - just free the surface.
                 if(outPointerToPixelData != NULL)
                     *outPointerToPixelData = NULL;
