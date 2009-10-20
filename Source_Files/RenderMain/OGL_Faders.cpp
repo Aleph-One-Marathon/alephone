bfd77ef3b2f96f7ea09464302b90d1f35f014896
Jeremiah Morris <hopper@whpress.com>
2009-10-20 02:50:24 +0000

    the texture coord array must be disabled before calling glDrawArrays (r3729 from shader-support branch)
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@3955 a75d40c9-0969-4759-85e5-97cb871b7ed4

--- Source_Files/RenderMain/OGL_Faders.cpp
+++ Source_Files/RenderMain/OGL_Faders.cpp
@@ -105,6 +105,7 @@ bool OGL_DoFades(float Left, float Top, float Right, float Bottom)
 	Vertices[2][1] = Bottom;
 	Vertices[3][0] = Left;
 	Vertices[3][1] = Bottom;
+	glDisableClientState(GL_TEXTURE_COORD_ARRAY);
 	glVertexPointer(2,GL_FLOAT,0,Vertices[0]);
 	
 	// Do real blending
@@ -224,6 +225,7 @@ bool OGL_DoFades(float Left, float Top, float Right, float Bottom)
 			break;
 		}		
 	}
+	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
 	
 	return true;
 }
