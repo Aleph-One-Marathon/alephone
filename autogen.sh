40efd7a4571922bff4d02c5fca48af0acc7561bd
Gregory Smith <wolfy@treellama.org>
2010-01-16 03:51:09 +0000

    revert OpenMP support--sometimes you lose
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@4154 a75d40c9-0969-4759-85e5-97cb871b7ed4

--- autogen.sh
+++ autogen.sh
@@ -44,7 +44,7 @@ fi
 
 rm -rf autom4te.cache
 
-aclocalinclude="-I m4 $ACLOCAL_FLAGS"; \
+aclocalinclude="$ACLOCAL_FLAGS"; \
 (echo $_echo_n " + Running aclocal: $_echo_c"; \
     aclocal $aclocalinclude; \
  echo "done.") && \
