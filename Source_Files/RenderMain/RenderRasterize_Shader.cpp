Aleph-One-Marathon/alephone

commit 2aab61f27b92b3bff9f042caa09bcb2341db99c9

Use corrected, consistent range for weapons flash in Lua HUD and shader APIs
git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@4267 a75d40c9-0969-4759-85e5-97cb871b7ed4

Source_Files/RenderMain/RenderRasterize_Shader.cpp
@@ -189,6 +189,8 @@ void RenderRasterize_Shader::setupGL() {
  */
 void RenderRasterize_Shader::render_tree() {
 
+	weaponFlare = PIN(view->maximum_depth_intensity - NATURAL_LIGHT_INTENSITY, 0, FIXED_ONE)/float(FIXED_ONE);
+
 	Shader* s = Shader::get(Shader::S_Invincible);
 	s->enable();
 	s->setFloat(Shader::U_Time, view->tick_count);
@@ -308,7 +310,7 @@ TextureManager RenderRasterize_Shader::setupSpriteTexture(const rectangle_defini
 	TMgr.IsShadeless = (rect.flags&_SHADELESS_BIT) != 0;
 	TMgr.TextureType = type;
 
-	float flare = view->maximum_depth_intensity/float(FIXED_ONE_HALF);
+	float flare = weaponFlare;
 
 	glEnable(GL_TEXTURE_2D);
 	glColor4f(color[0], color[1], color[2], 1);
@@ -316,12 +318,12 @@ TextureManager RenderRasterize_Shader::setupSpriteTexture(const rectangle_defini
 	switch(TMgr.TransferMode) {
 		case _static_transfer:
 			TMgr.IsShadeless = 1;
-			flare = 0;
+			flare = -1;
 			s = Shader::get(renderStep == kGlow ? Shader::S_InvincibleBloom : Shader::S_Invincible);
 			s->enable();
 			break;
 		case _tinted_transfer:
-			flare = 0;
+			flare = -1;
 			s = Shader::get(renderStep == kGlow ? Shader::S_InvisibleBloom : Shader::S_Invisible);
 			s->enable();
 			s->setFloat(Shader::U_Visibility, 1.0 - rect.transfer_data/32.0f);
@@ -336,7 +338,7 @@ TextureManager RenderRasterize_Shader::setupSpriteTexture(const rectangle_defini
 				} else {
 					glColor4f(0,0,0,1);
 				}
-				flare = 0;
+				flare = -1;
 			}
 			break;
 		default:
@@ -382,7 +384,7 @@ TextureManager RenderRasterize_Shader::setupWallTexture(const shape_descriptor& 
 	TMgr.IsShadeless = current_player->infravision_duration ? 1 : 0;
 	TMgr.TransferData = 0;
 
-	float flare = view->maximum_depth_intensity/float(FIXED_ONE_HALF);
+	float flare = weaponFlare;
 
 	glEnable(GL_TEXTURE_2D);
 	glColor4f(intensity, intensity, intensity, 1.0);
@@ -408,7 +410,7 @@ TextureManager RenderRasterize_Shader::setupWallTexture(const shape_descriptor& 
 				} else {
 					glColor4f(0,0,0,1);
 				}
-				flare = 0;
+				flare = -1;
 			}
 	}
 
@@ -518,7 +520,7 @@ void setupBlendFunc(short blendType) {
 	}
 }
 
-bool setupGlow(struct view_data *view, TextureManager &TMgr, float wobble, float intensity, float offset, RenderStep renderStep) {
+bool setupGlow(struct view_data *view, TextureManager &TMgr, float wobble, float intensity, float flare, float offset, RenderStep renderStep) {
 	if (TMgr.TransferMode == _textured_transfer && TMgr.IsGlowMapped()) {
 		Shader *s = NULL;
 		if (TMgr.TextureType == OGL_Txtr_Wall) {
@@ -543,7 +545,7 @@ bool setupGlow(struct view_data *view, TextureManager &TMgr, float wobble, float
 			s->setFloat(Shader::U_BloomScale, TMgr.GlowBloomScale());
 			s->setFloat(Shader::U_BloomShift, TMgr.GlowBloomShift());
 		}
-		s->setFloat(Shader::U_Flare, view->maximum_depth_intensity/float(FIXED_ONE_HALF));
+		s->setFloat(Shader::U_Flare, flare);
 		s->setFloat(Shader::U_Wobble, wobble);
 		s->setFloat(Shader::U_Depth, offset - 1.0);
 		s->setFloat(Shader::U_Glow, TMgr.MinGlowIntensity());
@@ -633,7 +635,7 @@ void RenderRasterize_Shader::render_node_floor_or_ceiling(clipping_window_data *
 
 		glDrawArrays(GL_POLYGON, 0, vertex_count);
 
-		if (setupGlow(view, TMgr, wobble, intensity, offset, renderStep)) {
+		if (setupGlow(view, TMgr, wobble, intensity, weaponFlare, offset, renderStep)) {
 			glDrawArrays(GL_POLYGON, 0, vertex_count);
 		}
 
@@ -781,7 +783,7 @@ void RenderRasterize_Shader::render_node_side(clipping_window_data *window, vert
 			
 			glDrawArrays(GL_QUADS, 0, vertex_count);
 
-			if (setupGlow(view, TMgr, wobble, intensity, offset, renderStep)) {
+			if (setupGlow(view, TMgr, wobble, intensity, weaponFlare, offset, renderStep)) {
 				glDrawArrays(GL_QUADS, 0, vertex_count);
 			}
 
@@ -874,11 +876,11 @@ bool RenderModel(rectangle_definition& RenderRectangle, short Collection, short 
 	bool canGlow = false;
 	switch(RenderRectangle.transfer_mode) {
 		case _static_transfer:
-			flare = 0;
+			flare = -1;
 			s = Shader::get(renderStep == kGlow ? Shader::S_InvincibleBloom : Shader::S_Invincible);
 			s->enable();
 		case _tinted_transfer:
-			flare = 0;
+			flare = -1;
 			s = Shader::get(renderStep == kGlow ? Shader::S_InvisibleBloom : Shader::S_Invisible);
 			s->enable();
 			s->setFloat(Shader::U_Visibility, 1.0 - RenderRectangle.transfer_data/32.0f);
@@ -893,7 +895,7 @@ bool RenderModel(rectangle_definition& RenderRectangle, short Collection, short 
 				} else {
 					glColor4f(0,0,0,1);
 				}
-				flare = 0;
+				flare = -1;
 			} else {
 				canGlow = true;
 			}
@@ -1157,7 +1159,7 @@ void RenderRasterize_Shader::render_node_object(render_object_data *object, bool
 
 	glDrawArrays(GL_QUADS, 0, 4);
 
-	if (setupGlow(view, TMgr, 0, 1, offset, renderStep)) {
+	if (setupGlow(view, TMgr, 0, 1, weaponFlare, offset, renderStep)) {
 		glDrawArrays(GL_QUADS, 0, 4);
 	}
 

Source_Files/Lua/lua_hud_objects.cpp
@@ -2314,7 +2314,7 @@ static int Lua_HUDLighting_Get_Ambient(lua_State *L)
 
 static int Lua_HUDLighting_Get_Weapon(lua_State *L)
 {
-    lua_pushnumber(L, PIN(current_player->weapon_intensity - 32768, 0, 32767)/32767.f);
+    lua_pushnumber(L, PIN(current_player->weapon_intensity - NATURAL_LIGHT_INTENSITY, 0, FIXED_ONE)/float(FIXED_ONE));
     return 1;
 }
 

Source_Files/RenderMain/OGL_Shader.cpp
@@ -435,9 +435,9 @@ void initDefaultPrograms() {
         "varying float FDxLOG2E;\n"
         "varying float MLxLOG2E;\n"
         "void main (void) {\n"
-        "	float flash = exp2((flare - 1.0) * 2.0);\n"
+        "	float flash = exp2(flare * 4.0);\n"
         "	float mlFactor = exp2(MLxLOG2E * dot(viewDir, viewDir) / flash + 1.0); \n"
-        "	mlFactor = clamp(mlFactor, 0.0, flare - 0.5) * 0.5;\n"
+        "	mlFactor = clamp(mlFactor, 0.0, flare + 0.5) * 0.5;\n"
         "	vec4 color = texture2D(texture0, gl_TexCoord[0].xy);\n"
         "	vec3 intensity = color.rgb * clamp(vertexColor.rgb + mlFactor, glow, 1.0);\n"
         "	float fogFactor = clamp(exp2(FDxLOG2E * length(viewDir)), 0.0, 1.0);\n"
@@ -562,9 +562,9 @@ void initDefaultPrograms() {
         "void main (void) {\n"
         "	vec3 texCoords = vec3(gl_TexCoord[0].xy, 0.0);\n"
         "	texCoords += vec3(normalize(viewXY).yx * wobble, 0.0);\n"
-        "	float flash = exp2((flare - 1.0) * 2.0);\n"
+        "	float flash = exp2(flare * 4.0);\n"
         "	float mlFactor = exp2(MLxLOG2E * dot(viewDir, viewDir) / flash + 1.0); \n"
-        "	mlFactor = clamp(mlFactor, 0.0, flare - 0.5) * 0.5;\n"
+        "	mlFactor = clamp(mlFactor, 0.0, flare + 0.5) * 0.5;\n"
         "	vec3 viewv = normalize(viewDir);\n"
         "	vec3 norm = vec3(0.0, 0.0, 1.0);\n"
         "	float diffuse = 0.5 + abs(dot(norm, viewv))*0.5;\n"
@@ -592,9 +592,9 @@ void initDefaultPrograms() {
         "void main (void) {\n"
         "	vec3 texCoords = vec3(gl_TexCoord[0].xy, 0.0);\n"
         "	texCoords += vec3(normalize(viewXY).yx * wobble, 0.0);\n"
-        "	float flash = exp2((flare - 1.0) * 2.0);\n"
+        "	float flash = exp2(flare * 4.0);\n"
         "	float mlFactor = exp2(MLxLOG2E * dot(viewDir, viewDir) / flash + 1.0); \n"
-        "	mlFactor = clamp(mlFactor, 0.0, flare - 0.5) * 0.5;\n"
+        "	mlFactor = clamp(mlFactor, 0.0, flare + 0.5) * 0.5;\n"
         "	vec3 viewv = normalize(viewDir);\n"
         "	vec3 norm = vec3(0.0, 0.0, 1.0);\n"
         "	float diffuse = 0.5 + abs(dot(norm, viewv))*0.5;\n"
@@ -625,9 +625,9 @@ void initDefaultPrograms() {
         "void main (void) {\n"
         "	vec3 texCoords = vec3(gl_TexCoord[0].xy, 0.0);\n"
         "	texCoords += vec3(normalize(viewXY).yx * wobble, 0.0);\n"
-        "	float flash = exp2((flare - 1.0) * 2.0);\n"
+        "	float flash = exp2(flare * 4.0);\n"
         "	float mlFactor = exp2(MLxLOG2E * dot(viewDir, viewDir) / flash + 1.0); \n"
-        "	mlFactor = clamp(mlFactor, 0.0, flare - 0.5) * 0.5;\n"
+        "	mlFactor = clamp(mlFactor, 0.0, flare + 0.5) * 0.5;\n"
         "	vec3 viewv = normalize(viewDir);\n"
         "	// iterative parallax mapping\n"
         "	float scale = 0.010;\n"
@@ -665,9 +665,9 @@ void initDefaultPrograms() {
         "void main (void) {\n"
         "	vec3 texCoords = vec3(gl_TexCoord[0].xy, 0.0);\n"
         "	texCoords += vec3(normalize(viewXY).yx * wobble, 0.0);\n"
-        "	float flash = exp2((flare - 1.0) * 2.0);\n"
+        "	float flash = exp2(flare * 4.0);\n"
         "	float mlFactor = exp2(MLxLOG2E * dot(viewDir, viewDir) / flash + 1.0); \n"
-        "	mlFactor = clamp(mlFactor, 0.0, flare - 0.5) * 0.5;\n"
+        "	mlFactor = clamp(mlFactor, 0.0, flare + 0.5) * 0.5;\n"
         "	vec3 viewv = normalize(viewDir);\n"
         "	// iterative parallax mapping\n"
         "	float scale = 0.010;\n"

Source_Files/RenderMain/RenderRasterize_Shader.h
@@ -21,6 +21,7 @@ class RenderRasterize_Shader : public RenderRasterizerClass {
 	Blur* blur;
 	int objectCount;
 	world_distance objectY;
+	float weaponFlare;
 
 protected:
 	virtual void render_node(sorted_node_data *node, bool SeeThruLiquids, RenderStep renderStep);	

commit 0261d685623f5275df5dadc809aca5139d54843b

Model lighting broke in r4267
git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@4398 a75d40c9-0969-4759-85e5-97cb871b7ed4

Source_Files/RenderMain/RenderRasterize_Shader.cpp
@@ -1088,7 +1088,7 @@ void RenderRasterize_Shader::render_node_object(render_object_data *object, bool
 		short collection = GET_COLLECTION(descriptor);
 		short clut = ModifyCLUT(rect.transfer_mode,GET_COLLECTION_CLUT(descriptor));
 
-		RenderModel(rect, collection, clut, view->maximum_depth_intensity/float(FIXED_ONE_HALF), renderStep);
+		RenderModel(rect, collection, clut, weaponFlare, renderStep);
 		glPopMatrix();
 		glDisable(GL_CLIP_PLANE5);
 		return;