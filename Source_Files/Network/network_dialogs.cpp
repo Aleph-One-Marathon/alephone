2142583de4d96d475430afaedd9cf66f1589e74c
Gregory Smith <wolfy@treellama.org>
2011-07-16 19:08:02 +0000

    Aleph One is now GPL 3
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@4455 a75d40c9-0969-4759-85e5-97cb871b7ed4

--- Source_Files/Network/network_dialogs.cpp
+++ Source_Files/Network/network_dialogs.cpp
@@ -6,7 +6,7 @@ NETWORK_DIALOGS.C  (network_dialogs.cpp)
  
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation; either version 2 of the License, or
+	the Free Software Foundation; either version 3 of the License, or
 	(at your option) any later version.
 
 	This program is distributed in the hope that it will be useful,
--- Source_Files/RenderMain/WorkQueue.h
+++ Source_Files/RenderMain/WorkQueue.h
@@ -4,7 +4,7 @@
 /*
     This class is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
+    the Free Software Foundation; either version 3 of the License, or
     (at your option) the most recent version.
 
     This class is distributed in the hope that it will be useful,
