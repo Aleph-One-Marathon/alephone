736f350c71e613863182d160e5cae9b87bbf683d
Gregory Smith <wolfy@treellama.org>
2011-07-29 02:12:05 +0000

    merge from trunk: remove unneeded jitter compensation
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/branches/release-0.23.3@4470 a75d40c9-0969-4759-85e5-97cb871b7ed4

--- Source_Files/Network/network_star_hub.cpp
+++ Source_Files/Network/network_star_hub.cpp
@@ -131,7 +131,7 @@ enum {
         kDefaultInGameWindowSize = TICKS_PER_SECOND * 5,
 	kDefaultPregameNthElement = 2,
 //	kDefaultInGameNthElement = kDefaultInGameWindowSize / 2,
-	kDefaultInGameNthElement = kDefaultInGameWindowSize - 1,
+	kDefaultInGameNthElement = 5,
         kDefaultPregameTicksBeforeNetDeath = 20 * TICKS_PER_SECOND,
         kDefaultInGameTicksBeforeNetDeath = 5 * TICKS_PER_SECOND,
 	kDefaultSendPeriod = 1,
@@ -798,13 +798,6 @@ hub_received_game_data_packet_v1(AIStream& ps, int inSenderIndex)
 	if(thePlayer.mOutstandingTimingAdjustment == 0 && thePlayer.mNthElementFinder.window_full())
 	{
 		thePlayer.mOutstandingTimingAdjustment = thePlayer.mNthElementFinder.nth_smallest_element((thePlayer.mSmallestUnheardTick >= sSmallestRealGameTick) ? sHubPreferences.mInGameNthElement : sHubPreferences.mPregameNthElement);
-
-		// if anti-lag is on, add a bit of latency based on jitter to try to keep it from triggering
-		if (sHubPreferences.mMinimumSendPeriod)
-		{
-			uint32 jitter = std::min(thePlayer.mNthElementFinder.nth_largest_element(0) - thePlayer.mNthElementFinder.nth_smallest_element(0) - (sHubPreferences.mMinimumSendPeriod - 1), (int32) 0);
-			thePlayer.mOutstandingTimingAdjustment -= jitter;
-		}
 
 		if(thePlayer.mOutstandingTimingAdjustment != 0)
 		{
