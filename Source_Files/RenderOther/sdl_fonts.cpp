Aleph-One-Marathon/alephone

commit 0c58356fc718ce6ff45dbda15e3e0cc937159131

If we can't find a scenario, ask the user
git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@4209 a75d40c9-0969-4759-85e5-97cb871b7ed4

Source_Files/shell.cpp
@@ -429,19 +429,31 @@ static void initialize_application(void)
 
 	init_physics_wad_data();
 
-	initialize_fonts();
-
 	// Parse MML files
 	SetupParseTree();
 	LoadBaseMMLScripts();
-	Plugins::instance()->enumerate();
 
 	// Check for presence of strings
 	if (!TS_IsPresent(strERRORS) || !TS_IsPresent(strFILENAMES)) {
 		fprintf(stderr, "Can't find required text strings (missing MML?).\n");
 		exit(1);
 	}
+	
+	// Check for presence of files (one last chance to change data_search_path)
+	if (!have_default_files()) {
+		char chosen_dir[256];
+		if (alert_choose_scenario(chosen_dir)) {
+			data_search_path.push_back(DirectorySpecifier(chosen_dir));
+			
+			// Parse MML files again, now that we have a new dir to search
+			SetupParseTree();
+			LoadBaseMMLScripts();
+		}
+	}
 
+	initialize_fonts();
+	Plugins::instance()->enumerate();			
+	
 	// Load preferences
 	initialize_preferences();
 #ifndef HAVE_OPENGL

Source_Files/CSeries/csalerts.h
@@ -42,6 +42,8 @@ extern void alert_user(
 	short item,
 	OSErr error);
 
+extern bool alert_choose_scenario(char *chosen_dir);
+
 extern void pause_debug(void);
 extern void vpause(
 	const char *message);

Source_Files/CSeries/csalerts.mm
@@ -40,3 +40,22 @@ void system_alert_user(const char* message, short severity)
 	[alert runModal];
 	[alert release];
 }
+
+bool system_alert_choose_scenario(char *chosen_dir)
+{
+	NSOpenPanel *panel = [NSOpenPanel openPanel];
+	[panel setCanChooseFiles:NO];
+	[panel setCanChooseDirectories:YES];
+	[panel setAllowsMultipleSelection:NO];
+	[panel setTitle:@"Choose Aleph One Scenario"];
+	[panel setMessage:@"Select an Aleph One scenario:"];
+	[panel setPrompt:@"Choose"];
+	
+	if (!chosen_dir)
+		return false;
+	
+	if ([panel runModal] != NSFileHandlingPanelOKButton)
+		return false;
+	
+	return [[[panel URL] path] getCString:chosen_dir maxLength:256 encoding:NSUTF8StringEncoding];
+}

Source_Files/CSeries/csalerts_sdl.cpp
@@ -38,6 +38,8 @@ April 22, 2003 (Woody Zenfell):
 
 #if defined(__WIN32__)
 #include <windows.h>
+#include <tchar.h>
+#include <shlobj.h>
 #endif
 
 /*
@@ -46,6 +48,7 @@ April 22, 2003 (Woody Zenfell):
 
 #ifdef __MACOSX__
 extern void system_alert_user(const char*, short);
+extern bool system_alert_choose_scenario(char *chosen_dir);
 #else
 void system_alert_user(const char* message, short severity)
 {
@@ -60,6 +63,31 @@ void system_alert_user(const char* message, short severity)
 #else
 	fprintf(stderr, "%s: %s\n", severity == infoError ? "INFO" : "FATAL", message);
 #endif	
+}
+bool system_alert_choose_scenario(char *chosen_dir)
+{
+#if defined(__WIN32__)
+	BROWSEINFO bi = { 0 };
+	TCHAR path[MAX_PATH];
+	bi.lpszTitle = _T("Select an Aleph One scenario:");
+	bi.pszDisplayName = path;
+	bi.ulFlags = BIF_RETURNONLYFSDIRS | BIF_NEWDIALOGSTYLE | 0x00000200; // no "New Folder" button
+	LPITEMIDLIST pidl = SHBrowseForFolder(&bi);
+	if (pidl)
+	{
+#ifdef UNICODE
+		WideCharToMultiByte(CP_UTF8, 0, path, -1, chosen_dir, 256, NULL, NULL);
+#else
+		strncpy(chosen_dir, path, 255);
+#endif
+		LPMALLOC pMalloc = NULL;
+		SHGetMalloc(&pMalloc);
+		pMalloc->Free(pidl);
+		pMalloc->Release();
+		return true;
+	}
+#endif
+	return false;
 }
 #endif
 
@@ -128,6 +156,12 @@ void alert_user(short severity, short resid, short item, OSErr error)
   }
   alert_user(msg, severity);
 }
+
+bool alert_choose_scenario(char *chosen_dir)
+{
+	return system_alert_choose_scenario(chosen_dir);
+}
+
 
 extern "C" void debugger(const char *message);
 

Source_Files/Files/preprocess_map_sdl.cpp
@@ -64,6 +64,14 @@ static bool get_default_spec(FileSpecifier &file, int type)
 	return get_default_spec(file, name);
 }
 
+bool have_default_files(void)
+{
+	FileSpecifier file;
+	return (get_default_spec(file, filenameDEFAULT_MAP) &&
+			get_default_spec(file, filenameIMAGES) &&
+			get_default_spec(file, filenameSHAPES8));
+}		
+
 void get_default_map_spec(FileSpecifier &file)
 {
 	if (!get_default_spec(file, filenameDEFAULT_MAP))

Source_Files/Misc/interface.h
@@ -423,6 +423,7 @@ void import_definition_structures(void);
 bool configure_key_setup(short *keycodes);
 
 /* --------- from PREPROCESS_MAP_MAC.C */
+bool have_default_files(void);
 void get_default_map_spec(FileSpecifier& File);
 void get_default_physics_spec(FileSpecifier& File);
 void get_default_sounds_spec(FileSpecifier& File);

commit e0e694fcaa58144f20613416cb17d7a65abea0b8

Ensure Fonts.fntA is available for interface font MML (broken in r4209)
git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@4817 a75d40c9-0969-4759-85e5-97cb871b7ed4

Source_Files/RenderOther/sdl_fonts.cpp
@@ -76,7 +76,7 @@ extern void fix_missing_overhead_map_fonts();
 extern void fix_missing_interface_fonts();
 #endif
 
-void initialize_fonts(void)
+void initialize_fonts(bool last_chance)
 {
         logContext("initializing fonts");
 	// Open font resource files
@@ -96,7 +96,7 @@ void initialize_fonts(void)
 		}
 		i++;
 	}
-	if (!found) {
+	if (!found && last_chance) {
 #ifdef HAVE_SDL_TTF
 		// use our own built-in font
 		fix_missing_overhead_map_fonts();

Source_Files/RenderOther/sdl_fonts.h
@@ -150,7 +150,7 @@ private:
  */
 
 // Initialize font management
-extern void initialize_fonts(void);
+extern void initialize_fonts(bool last_chance);
 
 // Load font, return pointer to font info
 extern font_info *load_font(const TextSpec &spec);

Source_Files/shell.cpp
@@ -542,6 +542,7 @@ static void initialize_application(void)
 	initialize_resources();
 
 	init_physics_wad_data();
+	initialize_fonts(false);
 
 	load_film_profile(FILM_PROFILE_DEFAULT, false);
 
@@ -566,12 +567,13 @@ static void initialize_application(void)
 			data_search_path.insert(data_search_path.begin() + dsp_insert_pos, chosen_dir);
 			
 			// Parse MML files again, now that we have a new dir to search
+			initialize_fonts(false);
 			SetupParseTree();
 			LoadBaseMMLScripts();
 		}
 	}
 
-	initialize_fonts();
+	initialize_fonts(true);
 	Plugins::instance()->enumerate();			
 	
 #if defined(__WIN32__) || (defined(__MACH__) && defined(__APPLE__))