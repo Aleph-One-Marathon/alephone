9be6f46f1f962b5d15741e6b5299bcbe8f3153fb
Jeremiah Morris <hopper@whpress.com>
2015-03-08 23:06:18 +0000

    Use Aleph One-specific URL for player settings page
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@5170 a75d40c9-0969-4759-85e5-97cb871b7ed4

--- Source_Files/Misc/alephversion.h
+++ Source_Files/Misc/alephversion.h
@@ -68,7 +68,7 @@ ALEPHVERSION.H
 #define A1_METASERVER_HOST "metaserver.lhowon.org"
 #define A1_METASERVER_LOGIN_URL "https://metaserver.lhowon.org/metaclient/login"
 #define A1_METASERVER_SIGNUP_URL "https://metaserver.lhowon.org/metaclient/signup"
-#define A1_METASERVER_SETTINGS_URL "https://metaserver.lhowon.org/settings"
+#define A1_METASERVER_SETTINGS_URL "https://metaserver.lhowon.org/metaclient/settings"
 #define A1_LEADERBOARD_URL "https://stats.lhowon.org/"
 #define A1_STATSERVER_ADD_URL "https://stats.lhowon.org/statclient/add"
 
