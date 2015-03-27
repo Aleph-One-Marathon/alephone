f40230b690e75b4353b88289665d910a6cb268b3
Jeremiah Morris <hopper@whpress.com>
2015-03-27 23:13:22 +0000

    Move some Mac defines to config.h, to be more like autoconf build
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/trunk@5205 a75d40c9-0969-4759-85e5-97cb871b7ed4

--- PBProjects/config.h
+++ PBProjects/config.h
@@ -1,50 +1,197 @@
-/* config.h.  Generated automatically by configure.  */
-/* config.h.in.  Generated automatically from configure.in by autoheader.  */
-/* acconfig.h
-   This file is in the public domain.
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
 
-   Descriptive text for the C preprocessor macros that
-   the distributed Autoconf macros can define.
-   No software package will use all of them; autoheader copies the ones
-   your configure.in uses into your configuration header file templates.
+/* ${desc_alsa} enabled */
+/* #undef HAVE_ALSA */
 
-   The entries are in sort -df order: alphabetical, case insensitive,
-   ignoring punctuation (such as underscores).  Although this order
-   can split up related entries, it makes it easier to check whether
-   a given entry is in the file.
+/* Define to 1 if you have the <alsa/asoundlib.h> header file. */
+/* #undef HAVE_ALSA_ASOUNDLIB_H */
 
-   Leave the following blank line there!!  Autoheader needs it.  */
-
+/* define if the Boost library is available */
+#define HAVE_BOOST /**/
 
-/* Define if OpenGL support is enabled. */
+/* define if the Boost::Filesystem library is available */
+#define HAVE_BOOST_FILESYSTEM /**/
+
+/* define if the Boost::System library is available */
+#define HAVE_BOOST_SYSTEM /**/
+
+/* ${desc_curl} enabled */
+#define HAVE_CURL 1
+
+/* ${desc_ffmpeg} enabled */
+#define HAVE_FFMPEG 1
+
+/* Define to 1 if you have the <GL/glext.h> header file. */
+#define HAVE_GL_GLEXT_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `asound' library (-lasound). */
+/* #undef HAVE_LIBASOUND */
+
+/* Define to 1 if you have the `mad' library (-lmad). */
+/* #undef HAVE_LIBMAD */
+
+/* Define to 1 if you have the `SDL_image' library (-lSDL_image). */
+#define HAVE_LIBSDL_IMAGE 1
+
+/* Define to 1 if you have the `SDL_net' library (-lSDL_net). */
+#define HAVE_LIBSDL_NET 1
+
+/* Define to 1 if you have the `SDL_ttf' library (-lSDL_ttf). */
+#define HAVE_LIBSDL_TTF 1
+
+/* Define to 1 if you have the `smpeg' library (-lsmpeg). */
+/* #undef HAVE_LIBSMPEG */
+
+/* Define to 1 if you have the `sndfile' library (-lsndfile). */
+/* #undef HAVE_LIBSNDFILE */
+
+/* Define to 1 if you have the `speex' library (-lspeex). */
+#define HAVE_LIBSPEEX 1
+
+/* Define to 1 if you have the `speexdsp' library (-lspeexdsp). */
+/* #undef HAVE_LIBSPEEXDSP */
+
+/* Define to 1 if you have the `z' library (-lz). */
+#define HAVE_LIBZ 1
+
+/* Lua support enabled */
+#define HAVE_LUA 1
+
+/* ${desc_mad} enabled */
+/* #undef HAVE_MAD */
+
+/* Define to 1 if you have the <mad.h> header file. */
+/* #undef HAVE_MAD_H */
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* OpenGL support enabled */
 #define HAVE_OPENGL 1
 
-/* Define if SDL_image support is enabled. */
+/* ${desc_png} enabled */
+#define HAVE_PNG 1
+
+/* Define to 1 if you have the <pwd.h> header file. */
+#define HAVE_PWD_H 1
+
+/* ${desc_sdl_image} enabled */
 #define HAVE_SDL_IMAGE 1
 
-/* Define if SDL_net support is enabled. */
-#define HAVE_SDL_NET 1
-
-
-/* Leave that blank line there!!  Autoheader needs it.
-   If you're adding to this file, keep in mind:
-   The entries are in sort -df order: alphabetical, case insensitive,
-   ignoring punctuation (such as underscores).  */
-
-/* Define if you have the <SDL_image.h> header file. */
+/* Define to 1 if you have the <SDL_image.h> header file. */
 #define HAVE_SDL_IMAGE_H 1
 
-/* Define if you have the <SDL_net.h> header file. */
+/* SDL_net support enabled */
+#define HAVE_SDL_NET 1
+
+/* Define to 1 if you have the <SDL_net.h> header file. */
 #define HAVE_SDL_NET_H 1
 
-/* Define if you have the <unistd.h> header file. */
+/* SDL_ttf support enabled */
+#define HAVE_SDL_TTF 1
+
+/* Define to 1 if you have the <SDL_ttf.h> header file. */
+#define HAVE_SDL_TTF_H 1
+
+/* ${desc_smpeg} enabled */
+/* #undef HAVE_SMPEG */
+
+/* Define to 1 if you have the <smpeg/smpeg.h> header file. */
+/* #undef HAVE_SMPEG_SMPEG_H */
+
+/* ${desc_sndfile} enabled */
+/* #undef HAVE_SNDFILE */
+
+/* Define to 1 if you have the <sndfile.h> header file. */
+/* #undef HAVE_SNDFILE_H */
+
+/* Define to 1 if you have the `snprintf' function. */
+#define HAVE_SNPRINTF 1
+
+/* ${desc_speex} enabled */
+#define HAVE_SPEEX 1
+
+/* Define to 1 if you have the <speex/speex.h> header file. */
+#define HAVE_SPEEX_SPEEX_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the `sysconf' function. */
+/* #undef HAVE_SYSCONF */
+
+/* Define to 1 if you have the `sysctlbyname' function. */
+#define HAVE_SYSCTLBYNAME 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
 #define HAVE_UNISTD_H 1
+
+/* ${desc_vorbis} enabled */
+/* #undef HAVE_VORBISFILE */
+
+/* Define to 1 if you have the `vsnprintf' function. */
+#define HAVE_VSNPRINTF 1
+
+/* zlib support enabled */
+#define HAVE_ZLIB 1
+
+/* Define to 1 if you have the <zlib.h> header file. */
+#define HAVE_ZLIB_H 1
+
+/* ${desc_zzip} enabled */
+#define HAVE_ZZIP 1
+
+/* mkstemp() available */
+#define LUA_USE_MKSTEMP 1
 
 /* Name of package */
 #define PACKAGE "AlephOne"
 
-/* Version number of package */
-#define VERSION "0.13.0"
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "http://sourceforge.net/p/marathon/bugs/"
 
-/* Define if the X Window System is missing or not being used. */
-/* #undef X_DISPLAY_MISSING */
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "Aleph One"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "Aleph One 20140104"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "AlephOne"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "http://marathon.sourceforge.net/"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "20140104"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Target platform name */
+#define TARGET_PLATFORM "darwin14.0.0 x86_64"
+
+/* Version number of package */
+#define VERSION "20140104"
+
+/* Win32 music disabled */
+/* #undef WIN32_DISABLE_MUSIC */
