afa60f4ab14f4b3b5a48fb7d78a1d7d404e0f7ca
Hopper262 <hopper@whpress.com>
2015-09-12 18:44:39 -0400

    remove unused leftovers from Vulcan

--- Source_Files/GameWorld/editor.h
+++ Source_Files/GameWorld/editor.h
@@ -32,40 +32,4 @@ Feb 12, 2000 (Loren Petrich):
 #define MARATHON_INFINITY_DATA_VERSION 2
 #define EDITOR_MAP_VERSION (MARATHON_INFINITY_DATA_VERSION)
 
-#define MINIMUM_MAP_X_COORDINATE SHORT_MIN
-#define MAXIMUM_MAP_X_COORDINATE SHORT_MAX
-#define MINIMUM_MAP_Y_COORDINATE SHORT_MIN
-#define MAXIMUM_MAP_Y_COORDINATE SHORT_MAX
-
-#define MINIMUM_FLOOR_HEIGHT (-8*WORLD_ONE)
-#define MINIMUM_CEILING_HEIGHT (MINIMUM_FLOOR_HEIGHT+WORLD_ONE)
-
-#define MAXIMUM_FLOOR_HEIGHT (8*WORLD_ONE)
-#define MAXIMUM_CEILING_HEIGHT (MAXIMUM_FLOOR_HEIGHT+WORLD_ONE)
-
-#define INVALID_HEIGHT (MINIMUM_FLOOR_HEIGHT-1)
-
-enum {
-	_saved_guard_path_is_random= 0x0001
-};
-
-struct map_index_data 
-{
-	char level_name[LEVEL_NAME_LENGTH];
-	char unused;
-	int32 level_flags;
-};
-
-#define MAXIMUM_GUARD_PATH_CONTROL_POINTS 20
-
-struct saved_path
-{
-	int16 point_count;
-	uint16 flags;
-	world_point2d points[MAXIMUM_GUARD_PATH_CONTROL_POINTS];
-	int16 polygon_indexes[MAXIMUM_GUARD_PATH_CONTROL_POINTS];
-};
-
-/* Prevent ridiculous maps.. */
-#define MAX_LINES_PER_VERTEX 15
 #endif
