ce62df50ac6e6165fb1a1a3c2cc33f05654acb53
Joshua Scoggins <Theoretically.x64@gmail.com>
2019-01-13 13:57:29 -0800

    Fix static inline template declarations to just be inline
    Violating the ODR is bad and it is even worse when templates are
    involved. Static templates in a header make absolutely no sense.

--- Source_Files/Files/Packing.h
+++ Source_Files/Files/Packing.h
@@ -90,7 +90,7 @@ extern void ValueToStream(uint8* &Stream, uint32 Value);
 extern void ValueToStream(uint8* &Stream, int32 Value);
 
 #ifndef PACKING_INTERNAL
-template<class T> inline static void StreamToList(uint8* &Stream, T* List, size_t Count)
+template<class T> inline void StreamToList(uint8* &Stream, T* List, size_t Count)
 {
     T* ValuePtr = List;
     for (size_t k=0; k<Count; k++)
@@ -98,20 +98,20 @@ template<class T> inline static void StreamToList(uint8* &Stream, T* List, size_
 }
 
 
-template<class T> inline static void ListToStream(uint8* &Stream, T* List, size_t Count)
+template<class T> inline void ListToStream(uint8* &Stream, T* List, size_t Count)
 {
     T* ValuePtr = List;
     for (size_t k=0; k<Count; k++)
         ValueToStream(Stream,*(ValuePtr++));
 }
 
-inline static void StreamToBytes(uint8* &Stream, void* Bytes, size_t Count)
+inline void StreamToBytes(uint8* &Stream, void* Bytes, size_t Count)
 {
     memcpy(Bytes,Stream,Count);
     Stream += Count;
 }
 
-inline static void BytesToStream(uint8* &Stream, const void* Bytes, size_t Count)
+inline void BytesToStream(uint8* &Stream, const void* Bytes, size_t Count)
 {
     memcpy(Stream,Bytes,Count);
     Stream += Count;
