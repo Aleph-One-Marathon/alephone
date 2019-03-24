Aleph-One-Marathon/alephone

commit eefa60f284d034fdc587b2b245a1140fb641b66e

Support high-dpi displays

Source_Files/RenderOther/HUDRenderer_Lua.cpp
@@ -105,6 +105,7 @@ blip_info HUD_Lua_Class::entity_blip(size_t index)
 void HUD_Lua_Class::start_draw(void)
 {
 	alephone::Screen *scr = alephone::Screen::instance();
+	scr->bound_screen();
     m_wr = scr->window_rect();
 	m_opengl = (get_screen_mode()->acceleration != _no_acceleration);
 	m_masking_mode = _mask_disabled;
@@ -132,8 +133,8 @@ void HUD_Lua_Class::start_draw(void)
 #endif
 	{
 		if (m_surface &&
-				(m_surface->w != MainScreenWidth() ||
-				 m_surface->h != MainScreenHeight()))
+				(m_surface->w != MainScreenLogicalWidth() ||
+				 m_surface->h != MainScreenLogicalHeight()))
 		{
 			SDL_FreeSurface(m_surface);
 			m_surface = NULL;
@@ -185,14 +186,6 @@ void HUD_Lua_Class::apply_clip(void)
     r.y = m_wr.y + scr->lua_clip_rect.y;
     r.w = MIN(scr->lua_clip_rect.w, m_wr.w - scr->lua_clip_rect.x);
     r.h = MIN(scr->lua_clip_rect.h, m_wr.h - scr->lua_clip_rect.y);
-#ifdef HAVE_OPENGL
-	if (m_opengl)
-	{
-        glEnable(GL_SCISSOR_TEST);
-        glScissor(r.x, scr->height() - r.y - r.h, r.w, r.h);
-	}
-	else
-#endif
 	if (m_surface)
 	{
 		SDL_SetClipRect(MainScreenSurface(), &r);

PBProjects/AppStore/Marathon 2/Info-MAS.plist
@@ -276,5 +276,7 @@
 	<array/>
 	<key>UTImportedTypeDeclarations</key>
 	<array/>
+	<key>NSHighResolutionCapable</key>
+	<true/>
 </dict>
 </plist>

PBProjects/AppStore/Marathon 2/Info.plist
@@ -276,5 +276,7 @@
 	<array/>
 	<key>UTImportedTypeDeclarations</key>
 	<array/>
+	<key>NSHighResolutionCapable</key>
+	<true/>
 </dict>
 </plist>

PBProjects/AppStore/Marathon Infinity/Info-MAS.plist
@@ -276,5 +276,7 @@
 	<array/>
 	<key>UTImportedTypeDeclarations</key>
 	<array/>
+	<key>NSHighResolutionCapable</key>
+	<true/>
 </dict>
 </plist>

PBProjects/AppStore/Marathon Infinity/Info.plist
@@ -276,5 +276,7 @@
 	<array/>
 	<key>UTImportedTypeDeclarations</key>
 	<array/>
+	<key>NSHighResolutionCapable</key>
+	<true/>
 </dict>
 </plist>

PBProjects/AppStore/Marathon/Info-MAS.plist
@@ -276,5 +276,7 @@
 	<array/>
 	<key>UTImportedTypeDeclarations</key>
 	<array/>
+	<key>NSHighResolutionCapable</key>
+	<true/>
 </dict>
 </plist>

PBProjects/AppStore/Marathon/Info.plist
@@ -268,5 +268,7 @@
 	<string>MainMenu</string>
 	<key>NSPrincipalClass</key>
 	<string>NSApplication</string>
+	<key>NSHighResolutionCapable</key>
+	<true/>
 </dict>
 </plist>

PBProjects/Info-AlephOne-Xcode4.plist
@@ -268,5 +268,7 @@
 	<string>MainMenu</string>
 	<key>NSPrincipalClass</key>
 	<string>NSApplication</string>
+	<key>NSHighResolutionCapable</key>
+	<true/>
 </dict>
 </plist>

commit 312e7eaee3e0c7c7f88570b4f1b0e5d0bf90f61c

Restore Lua HUD clip_rect
Regression introduced in eefa60f

Source_Files/RenderOther/HUDRenderer_Lua.cpp
@@ -186,6 +186,14 @@ void HUD_Lua_Class::apply_clip(void)
     r.y = m_wr.y + scr->lua_clip_rect.y;
     r.w = MIN(scr->lua_clip_rect.w, m_wr.w - scr->lua_clip_rect.x);
     r.h = MIN(scr->lua_clip_rect.h, m_wr.h - scr->lua_clip_rect.y);
+#ifdef HAVE_OPENGL
+	if (m_opengl)
+	{
+		glEnable(GL_SCISSOR_TEST);
+		glScissor(r.x * scr->pixel_scale(), (scr->height() - r.y - r.h) * scr->pixel_scale(), r.w * scr->pixel_scale(), r.h * scr->pixel_scale());
+	}
+	else
+#endif
 	if (m_surface)
 	{
 		SDL_SetClipRect(MainScreenSurface(), &r);