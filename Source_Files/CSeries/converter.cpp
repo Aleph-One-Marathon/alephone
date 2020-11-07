79fbdf6496a8c492471c105f411eccd6bcb7086f
root <root@DESKTOP-9BVH9QA.localdomain>
2020-11-07 16:36:21 +0900

    fix line wrap

--- Source_Files/CSeries/converter.cpp
+++ Source_Files/CSeries/converter.cpp
@@ -72,57 +72,56 @@ void sjisChar(const char* in, int* step, char* dst) {
 }
 
 
+
 // str is UTF-8
 std::vector<std::string> line_wrap(TTF_Font* t, const std::string& str,
 								   int size) {
-	std::vector<std::string> ret;
+
+	std::vector<std::string> blks;
 	std::string now;
-	utf8_iter it(str);
-	int w = 0;
-	while( ! it.end() ) {
+	bool in_space = false;
+	for(utf8_iter it(str); ! it.end(); ++it ) {
 		char32_t c = it.code();
-		int w2;
-		std::string tmp = it.utf8();
-		TTF_SizeUTF8(t, tmp.c_str(), &w2, nullptr);		
+		std::string s = it.utf8();
 		if( c >= 0x3040 && c <= 0x9fef ||
 			c >= 0x20000 && c <= 0x2ebe0 ) {
-			// fetch next letter
-			int w2;
-			if( w + w2 < size ) {
-				now += tmp;
-				w += w2;
-			} else {
-				ret.push_back(now);
-				now = tmp;
-				w = 0;
-			}
-		} else if( isspace(c) ) {
-			// don't care overflow
-			now += it.utf8();			
+			blks.push_back(now);
+			blks.push_back(s);
+			now.clear();
+		} else if( isspace(c)) {
+			in_space = true;
+			now += s;
 		} else {
-			// fetch next word
-			std::string tmp;
-			while( ! it.end() ) {
-				char32_t c = it.code();
-				if( isspace(c) ||
-					c >= 0x3040 && c <= 0x9fef ||
-					c >= 0x20000 && c <= 0x2ebe0 ) {
-					break;
-				} else {
-					tmp += it.utf8();
-				}
-			}
-			TTF_SizeUTF8(t, tmp.c_str(), &w2, 0);		
-			if( w > size ) {
-				ret.push_back(now);
-				now = tmp;
+			if( in_space ) {
+				blks.push_back(now);
+				now = s;
+				in_space = false;
 			} else {
-				now += tmp;
+				now += s;
 			}
+		}
+	}
+	if( ! now.empty() ) {
+		blks.push_back(now);
+	}
+	std::vector<std::string> ret;
+	now.clear();
+	int w = 0;
+	for(const auto& blk : blks) {
+		int w2;
+		TTF_SizeUTF8(t, blk.c_str(), &w2, nullptr);		
+		if( w + w2 < size ) {
+			now += blk;
+			w += w2;
+		} else {
+			ret.push_back(now);
+			now = blk;
+			w = w2;
 		}
 	}
 	if( ! now.empty() ) {
 		ret.push_back(now);
 	}
 	return ret;	
+	
 }
