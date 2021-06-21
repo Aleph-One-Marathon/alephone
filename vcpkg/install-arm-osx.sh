a49e3978ecae40cc44dc004dbafb7e07638c0e66
Gregory Smith <wolfy@treellama.org>
2021-06-20 21:34:02 -0400

    add vcpkg manifest support for Mac; including port and triplets overlay for apple silicon and 10.11 compatibility

---
+++ vcpkg/install-arm-osx.sh
@@ -0,0 +1,1 @@
+`cat ~/.vcpkg/vcpkg.path.txt`/vcpkg --overlay-ports=custom-ports --triplet=arm64-osx --x-install-root=installed-arm64-osx --x-manifest-root=../Source_Files --feature-flags="versions" install
---
+++ vcpkg/install-x64-osx.sh
@@ -0,0 +1,1 @@
+`cat ~/.vcpkg/vcpkg.path.txt`/vcpkg --overlay-triplets=custom-triplets --overlay-ports=custom-ports --triplet=x64-osx --x-install-root=installed-x64-osx --x-manifest-root=../Source_Files --feature-flags="versions" install
