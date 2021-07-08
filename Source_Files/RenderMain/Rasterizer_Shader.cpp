7848124fadd032163912c2bf7bba880dbc5eeaa7
Chris Hallock <christopherhallock@gmail.com>
2021-07-08 17:45:10 -0400

    Fix OpenGL classic-style perspective

--- Source_Files/RenderMain/Rasterizer_Shader.cpp
+++ Source_Files/RenderMain/Rasterizer_Shader.cpp
@@ -6,10 +6,6 @@
  *  http://www.gnu.org/licenses/gpl.html
  */
 
-#if defined (_MSC_VER) && !defined (M_PI)
-#define _USE_MATH_DEFINES
-#endif 
-
 #include "OGL_Headers.h"
 
 #include <iostream>
@@ -86,9 +82,7 @@ void Rasterizer_Shader_Class::SetView(view_data& view) {
 	float farVal = 128.0 * 1024.0;
 	float x = xtan * nearVal;
 	float y = ytan * nearVal;
-	float yoff = 0.0;
-	if (view.mimic_sw_perspective)
-		yoff = y * tan(pitch * M_PI/360.0) * 4.8;
+	float yoff = view.mimic_sw_perspective ? tan(pitch * deg2rad) * nearVal : 0;
 	glFrustum(-x, x, -y + yoff, y + yoff, nearVal, farVal);
 
 	glMatrixMode(GL_MODELVIEW);
