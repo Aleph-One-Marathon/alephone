edb1e860c7dc58d4ceb4707ade9e49950012844f
Gregory Smith <wolfy@treellama.org>
2022-07-02 10:56:40 -0400

    remove duplicate SDL_GL_SetAttribute calls

--- Source_Files/RenderOther/screen.cpp
+++ Source_Files/RenderOther/screen.cpp
@@ -903,7 +903,7 @@ static void change_screen_mode(int width, int height, int depth, bool nogl, bool
 		SDL_SetHint(SDL_HINT_OPENGL_ES_DRIVER, "1");
 		SDL_SetHint(SDL_HINT_VIDEO_WIN_D3DCOMPILER, "none");
 #endif
-		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
 		if (Get_OGL_ConfigureData().Multisamples > 0) {
@@ -914,10 +914,6 @@ static void change_screen_mode(int width, int height, int depth, bool nogl, bool
 			SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 0);
 		}
 		SDL_GL_SetSwapInterval(Get_OGL_ConfigureData().WaitForVSync ? 1 : 0);
-
-		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
-		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
 	}
 #endif
 #endif 
