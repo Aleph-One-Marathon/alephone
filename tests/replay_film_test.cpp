fba855a132e80e19bd157367077d2acea2d6606e
Kolfering <benoithauquier@hotmail.com>
2023-07-09 03:40:55 +0200

    Add piece of code to auto update file names with seed for replays UTs

--- tests/replay_film_test.cpp
+++ tests/replay_film_test.cpp
@@ -8,6 +8,15 @@ extern ShellOptions shell_options;
 
 using Replay = std::pair<std::string, uint16_t>; //replay file path and seed
 
+static uint16_t get_seed_from_filename(const std::string& file_name) {
+	auto position = file_name.find_last_of('.');
+	auto name_without_ext = file_name.substr(0, position);
+	auto seed_position = name_without_ext.find_last_of('.') + 1;
+	return stoi(name_without_ext.substr(seed_position));
+}
+
+#ifndef REPLAY_SET_SEED_FILENAME //enable and run this to set the correct file name with seed on new replay files
+
 static std::vector<Replay> get_replays(std::string& directory_path) {
 
 	FileSpecifier directory = directory_path;
@@ -20,14 +29,10 @@ static std::vector<Replay> get_replays(std::string& directory_path) {
 
 		FileSpecifier file = directory + it->name;
 
-		if (file.GetType() == _typecode_film) {
+		if (file.GetType() != _typecode_film) continue;
 
-			auto position = it->name.find_last_of('.');
-			auto name_without_ext = it->name.substr(0, position);
-			auto seed_position = name_without_ext.find_last_of('.') + 1;
-			uint16_t seed = stoi(name_without_ext.substr(seed_position));
-			results.push_back({ std::string(file.GetPath()), seed });
-		}
+		auto seed = get_seed_from_filename(it->name);
+		results.push_back({ std::string(file.GetPath()), seed });
 	}
 
 	return results;
@@ -53,3 +58,60 @@ TEST_CASE("Film replay") {
 
 	shutdown_application();
 }
+
+#else
+
+static std::vector<std::string> get_replays(std::string& directory_path) {
+
+	FileSpecifier directory = directory_path;
+
+	std::vector<dir_entry> entries;
+	directory.ReadDirectory(entries);
+
+	std::vector<string> results;
+	for (std::vector<dir_entry>::const_iterator it = entries.begin(); it != entries.end(); ++it) {
+
+		FileSpecifier file = directory + it->name;
+
+		if (file.GetType() != _typecode_film) continue;
+
+		try {
+			get_seed_from_filename(it->name);
+		} catch (...) {
+			results.push_back(file.GetPath());
+		}
+	}
+
+	return results;
+}
+
+TEST_CASE("Film replay set seed") {
+
+	REQUIRE(shell_options.replay_film_and_exit);
+	REQUIRE(!shell_options.directory.empty());
+	REQUIRE(!shell_options.replay_directory.empty());
+
+	const auto replays = get_replays(shell_options.replay_directory);
+
+	initialize_application();
+
+	for (const auto& replay : replays) {
+
+		REQUIRE(handle_open_document(replay));
+		main_event_loop();
+		auto seed = get_random_seed();
+		FileSpecifier file = replay;
+		std::string directory, file_name;
+		file.SplitPath(directory, file_name);
+		auto position = file_name.find_last_of('.');
+		auto name_without_ext = file_name.substr(0, position);
+		auto name_with_seed = name_without_ext + "." + std::to_string(seed) + ".filA";
+		FileSpecifier new_file = directory;
+		new_file.AddPart(name_with_seed);
+		REQUIRE(file.Rename(new_file));
+	}
+
+	shutdown_application();
+}
+
+#endif
