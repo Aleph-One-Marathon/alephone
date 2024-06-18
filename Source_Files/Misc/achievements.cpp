65eab2178f12cf0c0c83432b66def74e6b27ec58
Gregory Smith <wolfy@treellama.org>
2024-06-17 20:44:37 -0400

    Squashed commit of the following:
    commit b027fcd44b1d1cd923a94e5d39b644a94d2475a4
    Author: Gregory Smith <wolfy@treellama.org>
    Date:   Fri Jun 14 18:53:57 2024 -0400
    
        get updated MML for Marathon 2
    
    commit bb8cfddbfccf7bce90e72b8e2960fa763a59c0ad
    Author: Gregory Smith <wolfy@treellama.org>
    Date:   Fri Jun 14 18:50:47 2024 -0400
    
        don't show achievement disabled messages during replays or demos
    
    commit a922b957e6c8943b47d4aa414a62c1a0d9a0ffa4
    Author: Kolfering <benoithauquier@hotmail.com>
    Date:   Fri Jun 14 22:42:40 2024 +0200
    
        Use same intermediate directory for all steam games
    
    commit ea54c0a136bc30d27b9d4b854632c99c4f6f1986
    Author: Gregory Smith <wolfy@treellama.org>
    Date:   Fri Jun 14 15:39:45 2024 -0400
    
        include the Marathon 2 icon
    
    commit 6feec6228655f637b9a88ddb4cea61e4a190f116
    Author: Gregory Smith <wolfy@treellama.org>
    Date:   Fri Jun 14 14:38:54 2024 -0400
    
        add M2 steam workflows
    
    commit c49031ec9c467b8a42f4221ab885d050b2ae2400
    Author: Gregory Smith <wolfy@treellama.org>
    Date:   Fri Jun 14 14:28:20 2024 -0400
    
        add M2 achievements
    
    commit 85a64961ae3243430a711ad46c744893c0f20f6a
    Author: Gregory Smith <wolfy@treellama.org>
    Date:   Fri Jun 14 13:42:01 2024 -0400
    
        macOS build support
    
    commit 21ce42f43cd203351d4850a78bf68232cdebc535
    Author: Gregory Smith <wolfy@treellama.org>
    Date:   Fri Jun 14 12:23:43 2024 -0400
    
        add vdf files
    
    commit 37f383115dfc6796d9d4f63af1ff6cb29dc8151e
    Merge: 67ef957f 5538104e
    Author: Gregory Smith <wolfy@treellama.org>
    Date:   Fri Jun 14 12:13:06 2024 -0400
    
        Merge branch 'master' into marathon2-steam
    
    commit 67ef957ff2e9e395b31071ed53e036cec87dc493
    Author: Gregory Smith <wolfy@treellama.org>
    Date:   Fri Jun 14 12:04:07 2024 -0400
    
        add a target for Steam Marathon 2

--- Source_Files/Misc/achievements.cpp
+++ Source_Files/Misc/achievements.cpp
@@ -25,28 +25,49 @@ std::string Achievements::get_lua()
 	std::string lua;
 
 #ifdef HAVE_STEAM
-	if (!game_is_networked)
+	if (get_game_controller() == _single_player)
 	{
 		auto map_checksum = get_current_map_checksum();
 		auto physics_checksum = get_physics_file_checksum();
 		
 		static constexpr uint32_t m1_map_checksum = 0x03C9;
 		static constexpr uint32_t m1_physics_checksum = 0x5BC77666;
+
+		static constexpr uint32_t m2_map_checksum = 0x2d71ccb4;
+		static constexpr uint32_t m2_win95_map_checksum = 0x5e0ba590;
+		static constexpr uint32_t m2_physics_checksum = 0x91d72dab;
 		
-		if (map_checksum == m1_map_checksum &&
-			physics_checksum == m1_physics_checksum)
-		{
-			lua = 
-				#include "m1_achievements.lua"
-		;
+		switch (map_checksum) {
+			case m1_map_checksum:
+				if (physics_checksum == m1_physics_checksum)
+				{
+					lua =
+						#include "m1_achievements.lua"
+				;
+				}
+				else
+				{
+					set_disabled_reason("Achievements disabled (third party physics)");
+				}
+				break;
+			case m2_map_checksum:
+			case m2_win95_map_checksum:
+				if (physics_checksum == m2_physics_checksum)
+				{
+					lua =
+						#include "m2_achievements.lua"
+					;
+				}
+				else
+				{
+					set_disabled_reason("Achievements disabled (third party physics)");					
+				}
+				break;
 		}
-		else
+
+		if (lua.size() == 0)
 		{
-			if (physics_checksum != m1_physics_checksum)
-			{
-				set_disabled_reason("Achievements disabled (third party physics)");
-			}
-			logNote("achievements: invalidating due to checksum mismatch (map: 0x%x 0x%x, phy 0x%x 0x%x)", map_checksum, m1_map_checksum, physics_checksum, m1_physics_checksum);
+			logNote("achievements: invalidating due to checksum mismatch (map: 0x%x 0x%x, phy 0x%x 0x%x)", map_checksum, m1_map_checksum, physics_checksum, m1_physics_checksum);	
 		}
 	}
 #endif
