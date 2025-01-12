2ab94405ae53bcb5f5ad5f6c200e3c1fa17e33e3
Kolfering <benoithauquier@hotmail.com>
2025-01-12 16:00:58 +0100

    Add missing copyrights header on new audio files

--- Source_Files/Sound/MusicPlayer.cpp
+++ Source_Files/Sound/MusicPlayer.cpp
@@ -1,3 +1,21 @@
+/*
+    Copyright (C) 2023 Benoit Hauquier and the "Aleph One" developers.
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    This license is contained in the file "COPYING",
+    which is included with this source code; it is available online at
+    http://www.gnu.org/licenses/gpl.html
+*/
+
 #include "MusicPlayer.h"
 #include "OpenALManager.h"
 
