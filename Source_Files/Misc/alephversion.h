07a9c36aff403d143a0608815f725631a01e382c
Gregory Smith <wolfy@treellama.org>
2025-08-29 11:30:03 -0400

    version 1.11

--- Source_Files/Misc/alephversion.h
+++ Source_Files/Misc/alephversion.h
@@ -24,12 +24,12 @@ ALEPHVERSION.H
 
 
 #define A1_DISPLAY_NAME "Aleph One"
-#define A1_DISPLAY_VERSION "1.11b"
-#define A1_DISPLAY_DATE_VERSION "2025-08-08"
-#define A1_DATE_VERSION "20250808"
+#define A1_DISPLAY_VERSION "1.11"
+#define A1_DISPLAY_DATE_VERSION "2025-08-29"
+#define A1_DATE_VERSION "20250829"
 
 #ifdef _WIN32
-#define WIN_VERSION_STRING 0,2025,8,8 // <-- don't forget to update that for windows releases
+#define WIN_VERSION_STRING 0,2025,8,29 // <-- don't forget to update that for windows releases
 #define A1_DISPLAY_PLATFORM "Windows"
 #define A1_UPDATE_PLATFORM "windows"
 #elif defined (__APPLE__) && defined(__MACH__)
